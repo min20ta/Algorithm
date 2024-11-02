dc1055ca826ecabca2604299c0711c2529e90862
__ <127599615+min20ta@users.noreply.github.com>
2024-11-02 11:28:45 +0900

    Title: 트리의 지름, Time: 438ms, Memory: 176MB, Status: Memory Limit Exceed - CodeTree

---
+++ 241102/트리의 지름/diameter-of-tree.java
@@ -0,0 +1,94 @@
+import java.util.*;
+import java.io.*;
+
+
+
+
+public class Main {
+    static int n;
+
+    static ArrayList<ArrayList<Pair>> arrayList = new ArrayList<>();
+    static boolean [] visit;
+    static int [] dist;
+    static int longestV =Integer.MIN_VALUE;
+    static int [][] lenArray ;
+
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i <= n; i++) {
+            arrayList.add(new ArrayList<>());
+        }
+        lenArray = new int[n+1][n+1];
+
+        for (int i = 0; i < n - 1; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int len = Integer.parseInt(st.nextToken());
+
+            arrayList.get(a).add(new Pair(b,len));
+            arrayList.get(b).add(new Pair(a,len));
+
+
+        }
+
+        visit = new boolean[n+1];
+        dist = new int[n+1];
+
+        //가장 멀리있는 정점정보찾기
+        visit[1] = true;
+        dist[1] = 0;
+        dfs(1,0);
+
+        int longestDist = Integer.MIN_VALUE;
+        for (int i = 1; i <= n ; i++) {
+            if (dist[i]> longestDist){
+                longestDist = dist[i];
+                longestV = i;
+            }
+        }
+
+
+        visit = new boolean[n+1];
+        dist = new int[n+1];
+        visit[longestV] = true;
+        dist[longestV] = 0;
+        dfs(longestV,0);
+
+        longestDist = Integer.MIN_VALUE;
+        for (int i = 1; i <= n ; i++) {
+            if (dist[i]> longestDist){
+                longestDist = dist[i];
+                longestV = i;
+            }
+        }
+
+        System.out.println(longestDist);
+
+    }
+    static void dfs(int node, int totalDistance) {
+
+        for (Pair next : arrayList.get(node)) {
+            if (!visit[next.num]) {
+                visit[next.num] = true;
+                dist[next.num] = totalDistance+ next.dist;
+                dfs(next.num,dist[next.num]);
+            }
+        }
+    }
+
+    static class Pair {
+        int num;
+        int dist;
+
+        Pair(int num, int dist) {
+            this.num = num;
+            this.dist = dist;
+        }
+    }
+
+}
