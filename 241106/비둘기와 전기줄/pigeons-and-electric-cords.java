0b0e650873e4e5dda82316ad85ddfa659940796e
__ <127599615+min20ta@users.noreply.github.com>
2024-11-06 15:23:08 +0900

    Title: 비둘기와 전기줄, Time: 93ms, Memory: 8MB, Status: Runtime Error - CodeTree

---
+++ 241106/비둘기와 전기줄/pigeons-and-electric-cords.java
@@ -0,0 +1,37 @@
+import java.util.*;
+import java.io.*;
+
+
+public class Main {
+
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        List<ArrayList<Integer>> list = new ArrayList<>();
+        for (int i= 0 ; i <= n ; i++) {
+            list.add(new ArrayList<>());
+        }
+
+        for (int i = 0 ; i < n ; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            list.get(a).add(b);
+        }
+
+        int answer = 0;
+        for (int i = 0 ; i < n ; i++) {
+            for (int j = 1 ; j < list.get(i).size() ; j++) {
+                if(list.get(i).get(j) != list.get(i).get(j-1))
+                    answer++;
+            }
+        }
+        System.out.println(answer);
+
+
+
+    }
+    }
