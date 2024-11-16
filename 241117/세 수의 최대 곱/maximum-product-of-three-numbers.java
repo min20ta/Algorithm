f691c2230658152d9228f9c5ac1ddb0f637b27a1
__ <127599615+min20ta@users.noreply.github.com>
2024-11-17 01:09:32 +0900

    Title: 세 수의 최대 곱, Time: 280ms, Memory: 19MB, Status: Passed - CodeTree

--- 241117/세 수의 최대 곱/maximum-product-of-three-numbers.java
+++ 241117/세 수의 최대 곱/maximum-product-of-three-numbers.java
@@ -1,4 +1,3 @@
-import java.sql.SQLOutput;
 import java.util.*;
 import java.io.*;
 
@@ -11,12 +10,12 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int [] arr = new int[n];
+        long [] arr = new long[n];
         boolean isZero = false ;
         int isMinus = 0 ;
         int isPlus = 0 ;
         for (int i = 0; i < n; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
+            arr[i] = (long)Integer.parseInt(st.nextToken());
             if (arr[i] == 0)
                 isZero = true;
             else if (arr[i] > 0)
@@ -31,7 +30,8 @@ public class Main {
         // 양양양
         // 0
         //음수밖에 없을때: 작은것들
-        int max = Integer.MIN_VALUE;
+        // 음양 양
+        long max = Integer.MIN_VALUE;
         if (isMinus >= 2 && isPlus >=1) {
             max = Math.max(max, arr[0]*arr[1]*arr[n-1]);
         }
@@ -43,10 +43,10 @@ public class Main {
         if (isPlus == 0) {
             max = Math.max(max, arr[n-1]*arr[n-2]*arr[n-3]);
         }
+        else if (n==3)
+            max = Math.max(max, arr[0]*arr[1]*arr[2]);
 
         System.out.println(max);
 
     }
 }
-
-
