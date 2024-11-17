d9bd58c01e2bf3449dedb87ba6374a66124e853b
__ <127599615+min20ta@users.noreply.github.com>
2024-11-17 14:17:31 +0900

    Title: 최소 와이파이 수, Time: 86ms, Memory: 9MB, Status: Wrong Answer - CodeTree

--- 241117/최소 와이파이 수/minimum-number-of-wifi.java
+++ 241117/최소 와이파이 수/minimum-number-of-wifi.java
@@ -20,22 +20,30 @@ public class Main {
         }
 
         int ans = 0;
-        for (int i = m; i < n; i++) {
-            //지정자리 왼 :
-            int seat = arr[i];
-            int leftSum = 0;
-            for (int j = i- m ; j < i ; j++) {
-                leftSum += arr[j];
+        if (m == 0) {
+            for (int i = 0 ; i < n ; i++) {
+                if (arr[i] == 1)
+                    ans++;
             }
-            if (leftSum == 0){
-                continue;
-            }else if (leftSum > 0) {
-                ans++;
-                i+=m+1;
+        }else {
+            for (int i = m; i < n; i++) {
+                //지정자리 왼 :
+                int seat = arr[i];
+
+                int leftSum = 0;
+                for (int j = i - m; j < i; j++) {
+                    leftSum += arr[j];
+                }
+                if (leftSum == 0) {
+                    continue;
+                } else if (leftSum > 0) {
+                    ans++;
+                    i += m + m;
+                }
+
             }
-
-
         }
+
         System.out.println(ans);
 
 
