8fd7783c700eed5207bd600ca8745275e4ec44a1
__ <127599615+min20ta@users.noreply.github.com>
2024-11-17 15:14:04 +0900

    Title: 최소 와이파이 수, Time: 83ms, Memory: 9MB, Status: Passed - CodeTree

--- 241117/최소 와이파이 수/minimum-number-of-wifi.java
+++ 241117/최소 와이파이 수/minimum-number-of-wifi.java
@@ -21,51 +21,14 @@ public class Main {
         }
 
         int ans = 0;
-        if (m == 0) {
+
             for (int i = 0; i < n; i++) {
-                if (arr[i] == 1)
-                    ans++;
-            }
-        } else if (m >= n) {
-            for (int i = 0; i < n; i++) {
-                if (arr[i] == 1) {
-                    ans = 1;
-                    break;
-                }
-            }
-        } else {
-            for (int i = m; i < n; i++) {
                 //지정자리 왼 :
-                int seat = arr[i];
-                arr[i] = 0;
-                int leftSum = 0;
-
-                for (int j = i - m; j < i; j++) {
-                    leftSum += arr[j];
-                    arr[j] = 0;
-                }
-                for (int j = i+1 ; j <= i+m && j < n ; j++) {
-                    arr[j] = 0;
-                }
-
-                if (leftSum == 0) {
-                    continue;
-                } else if (leftSum > 0) {
+                if (arr[i] == 1) {
+                    i += 2 * m;
                     ans++;
-                    i += m+m ;
-
                 }
-
-                // 커버하지 못하고 남는 거
-
-
             }
-
-            for (int i = 0; i < n; i++) {
-                if (arr[i] == 1)
-                    ans++;
-            }
-        }
 
 
         System.out.println(ans);
@@ -77,6 +40,7 @@ public class Main {
 
     }
 }
+
 
 
 
