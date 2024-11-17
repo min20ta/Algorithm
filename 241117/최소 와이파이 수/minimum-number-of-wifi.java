ff32f1e261e7e65cc70b3433f60df1fe41a8a8c3
__ <127599615+min20ta@users.noreply.github.com>
2024-11-17 14:10:28 +0900

    Title: 최소 와이파이 수, Time: 87ms, Memory: 9MB, Status: Wrong Answer - CodeTree

---
+++ 241117/최소 와이파이 수/minimum-number-of-wifi.java
@@ -0,0 +1,49 @@
+
+import java.util.*;
+import java.io.*;
+
+
+public class Main {
+
+
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int [] arr = new int[n];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int ans = 0;
+        for (int i = m; i < n; i++) {
+            //지정자리 왼 :
+            int seat = arr[i];
+            int leftSum = 0;
+            for (int j = i- m ; j < i ; j++) {
+                leftSum += arr[j];
+            }
+            if (leftSum == 0){
+                continue;
+            }else if (leftSum > 0) {
+                ans++;
+                i+=m+1;
+            }
+
+
+        }
+        System.out.println(ans);
+
+
+
+
+
+
+    }
+}
+
+
