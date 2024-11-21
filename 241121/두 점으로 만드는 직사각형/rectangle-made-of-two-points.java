c3d39f43ed2ae41cfc6bea7703a39ebef62cbecb
__ <127599615+min20ta@users.noreply.github.com>
2024-11-21 16:42:39 +0900

    Title: 두 점으로 만드는 직사각형, Time: 92ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 241121/두 점으로 만드는 직사각형/rectangle-made-of-two-points.java
@@ -0,0 +1,35 @@
+
+import java.util.*;
+import java.io.*;
+
+
+public class Main {
+
+
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int x1 = Integer.parseInt(st.nextToken());
+        int y1 = Integer.parseInt(st.nextToken());
+        int x2 = Integer.parseInt(st.nextToken());
+        int y2 = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        int a1 = Integer.parseInt(st.nextToken());
+        int b1 = Integer.parseInt(st.nextToken());
+        int a2 = Integer.parseInt(st.nextToken());
+        int b2 = Integer.parseInt(st.nextToken());
+
+        int x0 = x1 < a1 ? x1 : a1;
+        int y0 = y1 < b1 ? y1 : b1;
+        int x00 = x2 < a2 ? a2 : x2;
+        int y00 = y2 < b2 ? b2 : y2;
+
+        System.out.println((x00-x0)*(y00-y0));
+      
+
+
+    }
+}
