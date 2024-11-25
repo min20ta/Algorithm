234465ac2bb4958a99b4d9451a976f1c40a5da1c
__ <127599615+min20ta@users.noreply.github.com>
2024-11-25 17:03:10 +0900

    Title: 상품 코드, Time: 99ms, Memory: 9MB, Status: Wrong Answer - CodeTree

---
+++ 241125/상품 코드/product-code.java
@@ -0,0 +1,45 @@
+
+import java.util.*;
+import java.io.*;
+
+
+public class Main {
+
+
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            String product= st.nextToken();
+            int code = Integer.parseInt(st.nextToken());
+
+            Game a = new Game();
+            a.product = "codetree";
+            a.code = 55;
+            Game b = new Game( product,code);
+
+
+            System.out.println("product " + a.code +" is "+a.product);
+            System.out.println("product " + b.code +" is "+b.product);
+
+
+    }
+}
+
+class Game {
+    String product;
+    int code;
+
+    Game() {
+        product = "";
+        code = 0;
+    }
+    Game(String product, int code) {
+        this.code = code;
+        this.product = product;
+    }
+}
+
+
+
