b8669c6d0c9caa1fcf0a3438da89cb39942aad14
__ <127599615+min20ta@users.noreply.github.com>
2024-12-11 12:27:13 +0900

    Title: 10진수로 변환하기, Time: 93ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 241211/10진수로 변환하기/convert-to-decimal.java
@@ -0,0 +1,20 @@
+
+import java.util.*;
+import java.io.*;
+
+
+public class Main {
+
+
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String num =br.readLine();
+
+
+        System.out.println(Integer.parseInt(num,2));
+
+
+    }
+}
