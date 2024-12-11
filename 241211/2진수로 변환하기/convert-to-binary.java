97410f5be7cb3641fa1d7c82c3677c6dc7aaae21
__ <127599615+min20ta@users.noreply.github.com>
2024-12-11 12:09:57 +0900

    Title: 2진수로 변환하기, Time: 94ms, Memory: 9MB, Status: Passed - CodeTree

--- 241211/2진수로 변환하기/convert-to-binary.java
+++ 241211/2진수로 변환하기/convert-to-binary.java
@@ -11,22 +11,23 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int num = Integer.parseInt(br.readLine());
-        int [] digits = new int[20];
-        int index = 0;
+        // int [] digits = new int[20];
+        // int index = 0;
 
-        while (true) {
-                if (num< 2) {
-                    digits[index] = num;
-                    break;
-                }
-               digits[index++] = num%2;
-               num/=2;
-        }
+        // while (true) {
+        //         if (num< 2) {
+        //             digits[index] = num;
+        //             break;
+        //         }
+        //        digits[index++] = num%2;
+        //        num/=2;
+        // }
 
-    for (int k = index ; k >= 0 ; k--) {
-        System.out.print(digits[k]);
-    }
+    // for (int k = index ; k >= 0 ; k--) {
+    //     System.out.print(digits[k]);
+    // }
 
+   System.out.println(Integer.toBinaryString(num));
 
     }
 }
