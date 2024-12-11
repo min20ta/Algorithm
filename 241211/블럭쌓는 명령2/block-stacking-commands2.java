9a19137c3ddadf17647c3e22ed52da2b12db40c7
__ <127599615+min20ta@users.noreply.github.com>
2024-12-11 15:57:50 +0900

    Title: 블럭쌓는 명령2, Time: 91ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 241211/블럭쌓는 명령2/block-stacking-commands2.java
@@ -0,0 +1,41 @@
+
+import java.util.*;
+import java.io.*;
+
+
+public class Main {
+
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        int [] arr = new int[n+1];
+
+        for (int i = 0; i < k; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            for (int j = a; j <=b ; j++) {
+                arr[j]++;                
+            }
+        }
+        
+        Arrays.sort(arr);
+
+        System.out.println(arr[n]);
+
+
+
+
+
+    }
+
+
+}
+
+
+
