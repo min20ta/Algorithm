3456d7fc40354f95377511d87bb60931c3ff6a9e
__ <127599615+min20ta@users.noreply.github.com>
2024-12-12 15:28:49 +0900

    Title: 사각형의 총 넓이 2, Time: 92ms, Memory: 9MB, Status: Passed - CodeTree

--- 241212/사각형의 총 넓이 2/total-width-of-a-rectangle2.java
+++ 241212/사각형의 총 넓이 2/total-width-of-a-rectangle2.java
@@ -10,32 +10,26 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int x1 = Integer.parseInt(st.nextToken()) + 100;
-        int y1 = Integer.parseInt(st.nextToken()) + 100;
-        int x2 = Integer.parseInt(st.nextToken()) + 100;
-        int y2 = Integer.parseInt(st.nextToken()) + 100;
-
-
-        st = new StringTokenizer(br.readLine());
-        int a1 = Integer.parseInt(st.nextToken()) + 100;
-        int b1 = Integer.parseInt(st.nextToken()) + 100;
-        int a2 = Integer.parseInt(st.nextToken()) + 100;
-        int b2 = Integer.parseInt(st.nextToken()) + 100;
-
         int [][] arr1 = new int[201][201];
 
-        for (int i = x1 ; i < x2 ; i++) {
-            for (int j = y1 ; j < y2 ; j++) {
-                arr1[i][j]= 1;
+        for (int i = 0; i < n; i++) {
+
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int x1 = Integer.parseInt(st.nextToken()) + 100;
+            int y1 = Integer.parseInt(st.nextToken()) + 100;
+            int x2 = Integer.parseInt(st.nextToken()) + 100;
+            int y2 = Integer.parseInt(st.nextToken()) + 100;
+
+
+            for (int k = x1 ; k < x2 ; k++) {
+                for (int j = y1 ; j < y2 ; j++) {
+                    arr1[k][j]= 1;
+                }
             }
+
         }
 
-        for (int i = a1 ; i < a2 ; i++) {
-            for (int j = b1 ; j < b2 ; j++) {
-                arr1[i][j]= 1;
-            }
-        }
+
         int sum = 0;
         for (int i = 0 ;i <= 200 ; i++) {
             for (int j = 0; j <= 200 ; j++) {
