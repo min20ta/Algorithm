688be53c5d907c0271a8d39aab5c1413248b41db
__ <127599615+min20ta@users.noreply.github.com>
2024-12-13 10:45:08 +0900

    Title: 연속되는 수 2, Time: 92ms, Memory: 9MB, Status: Runtime Error - CodeTree

--- 241213/연속되는 수 2/continuous-number2.java
+++ 241213/연속되는 수 2/continuous-number2.java
@@ -11,7 +11,7 @@ public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        int [] arr = new int[1001];
+        int [] arr = new int[n];
 
         for (int i = 0; i < n; i++) {
             int num = Integer.parseInt(br.readLine());
