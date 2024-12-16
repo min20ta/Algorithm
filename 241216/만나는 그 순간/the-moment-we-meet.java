1dbc501bb9109d5938f9b701505a97da079f3f04
__ <127599615+min20ta@users.noreply.github.com>
2024-12-16 12:31:42 +0900

    Title: 만나는 그 순간, Time: 93ms, Memory: 9MB, Status: Wrong Answer - CodeTree

---
+++ 241216/만나는 그 순간/the-moment-we-meet.java
@@ -0,0 +1,75 @@
+
+import java.util.*;
+import java.io.*;
+
+
+
+public class Main {
+
+
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int [] arrA = new int[1001];
+        int [] arrB = new int[1001];
+        int indexA = 0;
+        int indexB = 0;
+        int timeA = 0;
+        int timeB = 0;
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            char d = st.nextToken().charAt(0);
+            int num = Integer.parseInt(st.nextToken());
+
+            if (d == 'R') {
+                for (int j = indexA; j < indexA+num; j++) {
+                   arrA[timeA++] = j;
+                }
+                indexA+= num;
+            } else if (d == 'L') {
+                for (int j = indexA; j > indexA-num; j--){
+                    arrA[timeA++] = j;
+                }
+                indexA-= num;
+            }
+        }
+        arrA[timeA] = indexA;
+
+
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            char d = st.nextToken().charAt(0);
+            int num = Integer.parseInt(st.nextToken());
+
+            if (d == 'R') {
+                for (int j = indexB; j < indexB+num; j++) {
+                    arrB[timeB++] = j;
+                }
+                indexB+= num;
+            } else if (d == 'L') {
+                for (int j = indexB; j > indexB-num; j--){
+                    arrB[timeB++] = j;
+                }
+                indexB-= num;
+            }
+        }
+        arrB[timeB] = indexB;
+
+
+        int answer = -1;
+        for (int i = 1; i < 1001; i++) {
+
+            if (arrA[i] == arrB[i]) {
+                answer = i;
+                break;
+            }
+        }
+
+            System.out.println(answer);
+
+    }
+}
