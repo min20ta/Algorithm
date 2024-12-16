296df9be15b513c3cb5886b842789d28c6f6c999
__ <127599615+min20ta@users.noreply.github.com>
2024-12-16 17:33:49 +0900

    Title: 악수와 전염병의 상관관계 2, Time: 88ms, Memory: 9MB, Status: Wrong Answer - CodeTree

---
+++ 241216/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.java
@@ -0,0 +1,41 @@
+
+import java.util.*;
+import java.io.*;
+
+
+
+public class Main {
+
+
+ 
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        int p = Integer.parseInt(st.nextToken());
+        int t = Integer.parseInt(st.nextToken());
+
+        for (int i = 0; i < t; i++) {
+            st = new StringTokenizer(br.readLine());
+            int time = Integer.parseInt(st.nextToken());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+
+        }
+        //n명 개발자
+        //k. 1명 감염후 k번동안 만 전염
+        //p 처음 감염된 사람
+        //t
+        
+        //
+
+        //누가 최종적으로 전염병에 걸렸는지 출력
+        //전염 + 전염 -> 감염횟수+1. 재감염 x
+
+
+
+
+    }
+     
+}
