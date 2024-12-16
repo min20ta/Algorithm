3b1ee3b2812201f6639ec8caa390b98af9777664
__ <127599615+min20ta@users.noreply.github.com>
2024-12-16 13:51:59 +0900

    Title: 만나는 그 순간, Time: 89ms, Memory: 9MB, Status: Runtime Error - CodeTree

--- 241216/만나는 그 순간/the-moment-we-meet.java
+++ 241216/만나는 그 순간/the-moment-we-meet.java
@@ -62,7 +62,7 @@ public class Main {
 
         int answer = -1;
         // 같은거 처리 어케
-        for (int i = 1; i <= Math.max(timeA,timeB); i++) {
+        for (int i = 1; i <= Math.min(timeA,timeB); i++) {
             //System.out.println(arrA[i] + " "+arrB[i]);
             if (arrA[i] == arrB[i] ) {
                 answer = i;
