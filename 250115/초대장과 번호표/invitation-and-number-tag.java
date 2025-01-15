ac55ffe9ed8144e9ad133efd9894e26a053ef8bf
__ <127599615+min20ta@users.noreply.github.com>
2025-01-15 18:01:46 +0900

    Title: 초대장과 번호표, Time: 101ms, Memory: 9MB, Status: Runtime Error - CodeTree

--- 250115/초대장과 번호표/invitation-and-number-tag.java
+++ 250115/초대장과 번호표/invitation-and-number-tag.java
@@ -12,63 +12,53 @@ public class Main {
         int n = Integer.parseInt(st.nextToken());
         int g = Integer.parseInt(st.nextToken());//그룹
 
-        //n명. g그룹
-        //같은 사람이 동시에 여러그룹에 속 가능
-        //그룹내 모든 멤버가 정확히 일치x
-        //1번사람 무조건 초대장. k-1초대되었으면 마지막 사람도 초대됨
+        boolean [] invited = new boolean[n];
+        HashSet<Integer> [] groups = new HashSet[g];
+        ArrayList<Integer> [] peopleGroups = new ArrayList[n];
+        int ans = 0;
+        Queue<Integer> q = new LinkedList<>();
 
-        Set<Integer> set = new HashSet<>();
-        set.add(1);
+        for (int i = 0; i < g; i++) {
+            groups[i] = new HashSet<>();
+        }
 
-        ArrayList<ArrayList<Integer>> arrlist = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            peopleGroups[i] = new ArrayList<>();
+        }
 
         for (int i = 0; i < g; i++) {
             st = new StringTokenizer(br.readLine());
-            int num = Integer.parseInt(st.nextToken());
-            arrlist.add(new ArrayList<>());
-
-            for (int j = 0; j < num; j++) {
-                int people = Integer.parseInt(st.nextToken());
-                arrlist.get(i).add(people);
+            int s = Integer.parseInt(st.nextToken());
+            for (int j = 0; j < s; j++) {
+                int num = Integer.parseInt(st.nextToken());
+                groups[i].add(num); //그룹내 사람
+                peopleGroups[num].add(i);  //사람이 포함된 모든 그룹
             }
-            Collections.sort(arrlist.get(i));
         }
 
-        Collections.sort(arrlist, new Comparator<ArrayList<Integer>>() {
-            @Override
-            public int compare(ArrayList<Integer> o1, ArrayList<Integer> o2) {
-                if (o1.size() == o2.size()) {
-                    return o1.get(0) - o2.get(0);
+        q.add(1);
+        invited[1] = true;
+        while (!q.isEmpty()) { //사람한명씩 검사.
+            int x = q.poll();
+            ans++;
+
+            for (int i = 0; i < peopleGroups[x].size(); i++) { 
+                //for문을 돌지않고 한번에
+                //사람을 제거하기 위해서 hashSet사용
+                int gNum = peopleGroups[x].get(i);
+
+                groups[gNum].remove(x);
+
+                if (groups[gNum].size() == 1){
+                    int pNum = new ArrayList<>(groups[gNum]).get(0); //한명만 남은거 꺼내기위해서 리스트로 변환
+                    if (!invited[pNum]) {
+                        invited[pNum] = true;
+                        q.add(pNum);
+                    }
                 }
-                return o1.size() - o2.size();
-            }
-        });
 
-        for (int i = 0; i < g; i++) {
-
-            int num = arrlist.get(i).size();
-            int [] arr = new int[num];
-            int count = 0;
-            int idx = 0;
-
-            for (int j = 0; j < num; j++) {
-                int people = arrlist.get(i).get(j);
-                if (set.contains(people))
-                    count++;
-                else
-                    arr[idx++] = people;
-            }
-
-            if (count == num-1) {
-                set.add(arr[0]);
             }
         }
-
-        System.out.println(set.size());
-
-
-        //1,7,11,18,5,4,9,12,13,3,20,19,6
-        //1,7,11,18,5,4,12,13,9,3,20,6,19,
-
+        System.out.println(ans);
     }
 }
