ef1163ece8c6c4a2ad7667f97fcaa2dcb3d1e56a
__ <127599615+min20ta@users.noreply.github.com>
2025-01-15 16:27:29 +0900

    Title: 초대장과 번호표, Time: 196ms, Memory: 11MB, Status: Wrong Answer - CodeTree

--- 250115/초대장과 번호표/invitation-and-number-tag.java
+++ 250115/초대장과 번호표/invitation-and-number-tag.java
@@ -20,15 +20,39 @@ public class Main {
         Set<Integer> set = new HashSet<>();
         set.add(1);
 
+        ArrayList<ArrayList<Integer>> arrlist = new ArrayList<>();
+
         for (int i = 0; i < g; i++) {
             st = new StringTokenizer(br.readLine());
             int num = Integer.parseInt(st.nextToken());
+            arrlist.add(new ArrayList<>());
+
+            for (int j = 0; j < num; j++) {
+                int people = Integer.parseInt(st.nextToken());
+                arrlist.get(i).add(people);
+            }
+            Collections.sort(arrlist.get(i));
+        }
+
+        Collections.sort(arrlist, new Comparator<ArrayList<Integer>>() {
+            @Override
+            public int compare(ArrayList<Integer> o1, ArrayList<Integer> o2) {
+                if (o1.size() == o2.size()) {
+                    return o1.get(0) - o2.get(0);
+                }
+                return o1.size() - o2.size();
+            }
+        });
+
+        for (int i = 0; i < g; i++) {
+
+            int num = arrlist.get(i).size();
             int [] arr = new int[num];
             int count = 0;
             int idx = 0;
 
             for (int j = 0; j < num; j++) {
-                int people = Integer.parseInt(st.nextToken());
+                int people = arrlist.get(i).get(j);
                 if (set.contains(people))
                     count++;
                 else
@@ -41,7 +65,10 @@ public class Main {
         }
 
         System.out.println(set.size());
-        //13,7,20,3,1,
+
+
+        //1,7,11,18,5,4,9,12,13,3,20,19,6
+        //1,7,11,18,5,4,12,13,9,3,20,6,19,
 
     }
 }
