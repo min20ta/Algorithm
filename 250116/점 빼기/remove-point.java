4615e65538356e1d71fb01f5805c564ddafa2870
__ <127599615+min20ta@users.noreply.github.com>
2025-01-16 19:25:17 +0900

    Title: 점 빼기, Time: 832ms, Memory: 21MB, Status: Passed - CodeTree

---
+++ 250116/점 빼기/remove-point.java
@@ -0,0 +1,55 @@
+
+import java.util.*;
+import java.io.*;
+
+
+public class Main {
+
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n= Integer.parseInt(st.nextToken());
+        int m= Integer.parseInt(st.nextToken());
+        TreeSet<pair> set = new TreeSet<>();
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            set.add(new pair(x,y));
+        }
+
+        for (int i = 0; i < m; i++) {
+            int k = Integer.parseInt(br.readLine());
+            if (set.ceiling(new pair(k,0))!= null) {
+                pair p = set.ceiling(new pair(k,0));
+                System.out.println(p.x+" "+p.y);
+                set.remove(p);
+            }else {
+                System.out.println("-1 -1");
+            }
+        }
+
+    }
+
+    static class pair implements Comparable<pair> {
+        int x;
+        int y;
+
+        pair(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+
+        @Override
+        public int compareTo(pair p) {
+            if (this.x == p.x) {
+                return this.y - p.y;
+            }
+            return this.x - p.x;
+
+        }
+    }
+}
