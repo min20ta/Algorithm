885f1cd58e5b293f59db1b52bcd83ecacb0261d5
__ <127599615+min20ta@users.noreply.github.com>
2025-01-19 02:17:21 +0900

    Title: 길이가 무한대인 수평선 트랙, Time: 101ms, Memory: 9MB, Status: Wrong Answer - CodeTree

---
+++ 250119/길이가 무한대인 수평선 트랙/horizontal-track-with-infinite-length.java
@@ -0,0 +1,66 @@
+
+import java.util.*;
+import java.io.*;
+
+
+public class Main {
+
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int t = Integer.parseInt(st.nextToken());
+
+        TreeSet<pair> set = new TreeSet<>();
+
+        //만나면 remove
+        //분 -> 시간초과ㅠ
+        //중복제거,
+        int ans = 0;
+
+        st = new StringTokenizer(br.readLine());
+        int p = Integer.parseInt(st.nextToken());
+        int v = Integer.parseInt(st.nextToken());
+
+        int end = p + v * t;
+        set.add(new pair(p, v, end));
+        ans++;
+
+
+        for (int i = 1; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            int p2 = Integer.parseInt(st.nextToken());
+            int v2 = Integer.parseInt(st.nextToken());
+
+            int end2 = p2 + v2 * t;
+            if (set.last().end < end2) {
+                set.add(new pair(p2, v2, end2));
+                ans++;
+            }
+
+
+
+        }
+        System.out.println(ans);
+    }
+
+        static class pair implements Comparable<pair> {
+        int person;
+        int velocity;
+        int end;
+
+        pair(int person, int velocity,int end) {
+            this.person = person;
+            this.velocity = velocity;
+            this.end = end;
+        }
+
+        @Override
+            public int compareTo(pair p) {
+
+            return this.end - p.end;
+        }
+        }
+
+}
