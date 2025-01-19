cf244a01f6288f5eea3c3aac390ef0269db5dbae
__ <127599615+min20ta@users.noreply.github.com>
2025-01-19 20:22:42 +0900

    Title: 돌 잘 치우기, Time: 91ms, Memory: 9MB, Status: Passed - CodeTree

--- 250119/돌 잘 치우기/clear-stones-well.java
+++ 250119/돌 잘 치우기/clear-stones-well.java
@@ -13,7 +13,9 @@ public class Main {
     static int n;
     static int k;
     static int m;
-    static ArrayList<pair> arrayList;
+    static ArrayList<pair> sPos = new ArrayList<>();
+    static ArrayList<pair> stonePos = new ArrayList<>();
+    static ArrayList<pair> selected = new ArrayList<>();
     static int max = Integer.MIN_VALUE;
     static  Queue<pair> queue = new LinkedList<>();
 
@@ -24,7 +26,7 @@ public class Main {
         k = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
         arr = new int[n+1][n+1];
-        arrayList = new ArrayList<>();
+
 
 
         for (int i = 1; i <= n; i++) {
@@ -32,6 +34,8 @@ public class Main {
             for (int j = 1; j <= n; j++) {
                 int num = Integer.parseInt(st.nextToken());
                 arr[i][j] = num;
+                if (arr[i][j] == 1)
+                    stonePos.add(new pair(i,j));
             }
         }
 
@@ -39,13 +43,10 @@ public class Main {
             st = new StringTokenizer(br.readLine());
             int r = Integer.parseInt(st.nextToken());
             int c = Integer.parseInt(st.nextToken());
-            arrayList.add(new pair(r,c));
+            sPos.add(new pair(r,c));
         }
 
-        //돌제거후 매번 bfs시행.
-        //dfs를 같이 써야하나???
-
-        dfs(0);
+        findMax(0,0);
         System.out.println(max);
 
     }
@@ -60,7 +61,6 @@ public class Main {
                 if (x >= 1 && x <= n && y >= 1 && y <= n) {
                     if (!visit[x][y] && arr[x][y] == 0 ) {
                         visit[x][y] = true;
-                        arr2[x][y] = 2;
                         queue.add(new pair(x,y));
                     }
                 }
@@ -69,45 +69,50 @@ public class Main {
 
     }
 
-    static void dfs(int depth) {
-        if (depth == m) {
-            arr2 = new int[n+1][n+1];
-            visit = new boolean[n+1][n+1];
-            
-            for (int i = 0; i < k; i++) {
-                pair p = arrayList.get(i);
-                int r = p.x;
-                int c = p.y;
-                visit[r][c] = true;
-                arr2[r][c] = 2;
-                queue.add(new pair(r,c));
-            }
-            
-            bfs();
-            
-            int sum = 0;
-            for (int i = 1; i <= n; i++) {
-                for (int j = 1; j <= n ; j++) {
-                    if (arr2[i][j] == 2)
-                        sum++;
-                }
-            }
+    static void findMax(int idx, int cnt) {
+      if (idx == stonePos.size()){
+          if (cnt == m)
+              max = Math.max(max,calc());
+          return;
+      }
+      selected.add(stonePos.get(idx));
+      findMax(idx+1,cnt+1);
+      selected.remove(selected.size()-1);
+      findMax(idx+1,cnt);
 
-            max = Math.max(max, sum);
+    }
 
-            return;
+    static int calc() {
+        for (int i = 0; i < m; i++) {
+            int x = selected.get(i).x;
+            int y = selected.get(i).y;
+            arr[x][y] = 0;
+        }
+        visit = new boolean[n+1][n+1];
+
+        for (int i = 0; i < k; i++) {
+                queue.add(sPos.get(i));
+                visit[sPos.get(i).x][sPos.get(i).y] = true;
+        }
+        bfs();
+
+        int sum = 0;
+        for (int i = 1; i <= n  ; i++) {
+            for (int j =1 ; j <= n ; j++) {
+                if (visit[i][j])
+                    sum++;
+            }
         }
 
-        for (int i = 1 ; i <= n ; i++) {
-            for (int j = 1 ; j <= n ; j++) {
-                if (arr[i][j] == 1) {
-                    arr[i][j] = 0;
-                    dfs(depth+1);
-                    arr[i][j] = 1;
-                }
-
-            }
+        for (int i = 0; i < m; i++) {
+            int x = selected.get(i).x;
+            int y = selected.get(i).y;
+            arr[x][y] = 1;
         }
+
+
+        return sum;
+
     }
     static class pair  {
         int x;
