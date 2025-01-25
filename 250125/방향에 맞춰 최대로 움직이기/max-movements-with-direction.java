f417da4d6dd3230b1d4cb2ba82062a2bc863f9c5
__ <127599615+min20ta@users.noreply.github.com>
2025-01-25 16:01:34 +0900

    Title: 방향에 맞춰 최대로 움직이기, Time: 85ms, Memory: 9MB, Status: Wrong Answer - CodeTree

---
+++ 250125/방향에 맞춰 최대로 움직이기/max-movements-with-direction.java
@@ -0,0 +1,105 @@
+
+import java.util.*;
+import java.io.*;
+
+
+public class Main {
+
+    static int n ;
+    static int [][] arr;
+    static int [][] dirArr;
+    static int r;
+    static int c;
+
+    static int max = Integer.MIN_VALUE;
+    static ArrayList<Integer> arrayList = new ArrayList<>();
+    static Queue<pair> queue = new LinkedList<>();
+
+    static int [] dx = {0,-1,-1,0,1,1,1,0,-1};
+    static int [] dy = {0,0,1,1,1,0,-1,-1,-1};
+
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+
+        arr = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < n; j++) {
+                int num = Integer.parseInt(st.nextToken());
+                arr[i][j] = num;
+            }
+        }
+
+        dirArr = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < n; j++) {
+                int num = Integer.parseInt(st.nextToken());
+                dirArr[i][j] = num;
+            }
+        }
+
+        st = new StringTokenizer(br.readLine());
+        r = Integer.parseInt(st.nextToken())-1;
+        c = Integer.parseInt(st.nextToken())-1;
+
+
+
+        //시작점에서 방향에 있는 숫자 중 더 큰것만 가능
+        queue.add(new pair(r,c,0));
+        findMax();
+        System.out.println(max);
+
+        }
+
+        static void findMax() {
+
+
+
+            while(!queue.isEmpty()) {
+                pair p = queue.poll();
+                int newx = p.x;
+                int newy = p.y;
+                int dir = dirArr[newx][newy];
+
+
+                while(true) {
+                    int x = newx + dx[dir];
+                    int y = newy + dy[dir];
+
+                    if (x >= 0 && x < n && y >= 0 && y < n) {
+                        if (arr[x][y] > arr[p.x][p.y]) {
+                            //System.out.print(x+" "+y+" ");
+                            max = Math.max(max, p.count + 1);
+                            //System.out.print(p.count+1+" ");
+                            //System.out.println();
+                            queue.add(new pair(x, y, p.count + 1));
+                        }
+                        newx = x;
+                        newy = y;
+                    }else
+                        break;
+                }
+            }
+
+        }
+
+        static class pair {
+        int x;
+        int y;
+        int count;
+
+        pair(int x, int y, int count) {
+            this.x = x;
+            this.y = y;
+            this.count = count;
+        }
+        }
+    }
+
+
+
+
