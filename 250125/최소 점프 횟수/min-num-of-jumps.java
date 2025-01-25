101a1e387ab52fa26830de0fa27f0d2e682d7d03
__ <127599615+min20ta@users.noreply.github.com>
2025-01-25 19:03:47 +0900

    Title: 최소 점프 횟수, Time: 88ms, Memory: 9MB, Status: Runtime Error - CodeTree

---
+++ 250125/최소 점프 횟수/min-num-of-jumps.java
@@ -0,0 +1,64 @@
+
+import java.util.*;
+import java.io.*;
+
+
+public class Main {
+
+    static int n ;
+    static int [] arr;
+    static int [] ansArr;
+    static ArrayList<Integer> arrayList = new ArrayList<>();
+    static int min = Integer.MAX_VALUE;
+
+
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        arr = new int[n];
+        ansArr = new int[n];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            int num = Integer.parseInt(st.nextToken());
+            arr[i] = num;
+        }
+
+        //최소점프수
+        //최대 점프가능 거리
+        findMin(0,0,0);
+        if (min == Integer.MAX_VALUE)
+            System.out.println(-1);
+        else
+            System.out.println(min);
+    }
+
+    static void findMin(int idx, int prev, int jump){
+
+        if (idx == n-1){
+            min = Math.min(min, jump);
+            return;
+        }
+
+
+        for (int i = 1 ; i <= arr[prev] ; i++) {
+            if (prev + i > n)
+                return;
+
+            arrayList.add(prev+i);
+            findMin(prev+i, prev+i, jump+1);
+            arrayList.remove(arrayList.size()-1);
+
+        }
+
+
+
+
+
+    }    }
+
+
+
+
