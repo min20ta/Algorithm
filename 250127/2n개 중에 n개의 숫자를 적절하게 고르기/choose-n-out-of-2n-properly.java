101aa1765e7aa281ad5061df1ff0633fe7202869
__ <127599615+min20ta@users.noreply.github.com>
2025-01-27 00:39:26 +0900

    Title: 2n개 중에 n개의 숫자를 적절하게 고르기, Time: 82ms, Memory: 9MB, Status: Wrong Answer - CodeTree

---
+++ 250127/2n개 중에 n개의 숫자를 적절하게 고르기/choose-n-out-of-2n-properly.java
@@ -0,0 +1,66 @@
+
+import java.util.*;
+import java.io.*;
+
+
+public class Main {
+
+    static int n ;
+
+    static int [] arr;
+    static ArrayList<Integer> arrayList = new ArrayList<>();
+    static int min = Integer.MAX_VALUE;
+    static int sumAll = 0;
+
+
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+
+        arr = new int[2 * n];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < 2 * n; i++) {
+            int num = Integer.parseInt(st.nextToken());
+            arr[i] = num;
+            sumAll+= num;
+        }
+        //n개씩 2개그룹]
+        findMin(0);
+        System.out.println(min);
+
+    }
+    static void findMin(int depth) {
+
+
+        if (depth == n){
+            int sum = 0;
+            for (int i = 0; i < n; i++) {
+                sum+= arrayList.get(i);
+            }
+            int sum2 = sumAll - sum;
+            int diff = Math.abs(sum - sum2);
+            min = Math.min(min, diff);
+
+            return;
+
+    }
+        //정해진 개수가 있을때
+        for (int i = 0; i < 2*n; i++) {
+            arrayList.add(arr[i]);
+            findMin(depth+1);
+            arrayList.remove(arrayList.size()-1);
+        }
+
+        }
+
+
+        }
+
+
+
+
+
+
+
