4a7abaaef8ebb0ec6047687747e1bb3b26fbfbd7
__ <127599615+min20ta@users.noreply.github.com>
2025-01-26 17:07:45 +0900

    Title: 가능한 수열 중 최솟값 구하기, Time: 99ms, Memory: 9MB, Status: Passed - CodeTree

--- 250126/가능한 수열 중 최솟값 구하기/find-min-of-possible-series.java
+++ 250126/가능한 수열 중 최솟값 구하기/find-min-of-possible-series.java
@@ -29,43 +29,50 @@ public class Main {
     }
     static void findMin(int count) {
         if (count == n) {
-            if (possible()) {
                 for (int i = 0; i < n ; i++) {
                     System.out.print(arrayList.get(i));
                 }
                 System.exit(0);
-            }
 
             return;
         }
 
         for (int i = 4; i <=6 ; i++) {
-            if (arrayList.size()>0 && arrayList.get(arrayList.size()-1) == i)
-                continue;
+
             arrayList.add(i);
-            findMin(count+1);
+            if (possible())
+                findMin(count+1);
             arrayList.remove(arrayList.size()-1);
         }
     }
 
+    //재귀를 조건으로 컷팅해준다
+
     static boolean possible() {
         //n은 80까지
+        int len = 1;
+        while (true) {
+            int end1 = arrayList.size()-1, start1 = end1-len+1;
+            int end2 = start1-1, start2 = end2-len+1;
 
-        for (int i = 1; i <= n/2; i++) {
+            if (start2 < 0)
+                break;
 
-            for (int j = 0; j < n; j++) {
-                int count = 0;
+            if (isEqual(start1, end1, start2, end2))
+                return false;
 
-                for (int k = j; k < j+i && k+i < n; k++) {
-                    if (arrayList.get(k) == arrayList.get(k+i)){
-                        count++;
-                    }
-                }
-                if (count == i)
-                    return false;
-            }
+            len++;
+
         }
 
+        return true;
+    }
+
+    static boolean isEqual(int start1, int end1, int start2, int end2) {
+        for (int i = 0; i <= end1-start1; i++) {
+            if (arrayList.get(start1+i) != arrayList.get(start2 + i))
+                return false;
+        }
         return true;
     }
 
