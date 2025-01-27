00da6b5815ece26da132c8b1db9a848efa3949c0
__ <127599615+min20ta@users.noreply.github.com>
2025-01-27 19:59:53 +0900

    Title: n개의 점 중 m개 고르기, Time: 89ms, Memory: 9MB, Status: Wrong Answer - CodeTree

--- 250127/n개의 점 중 m개 고르기/choose-m-out-of-n-points.java
+++ 250127/n개의 점 중 m개 고르기/choose-m-out-of-n-points.java
@@ -14,8 +14,6 @@ public class Main {
     static double min = Double.MAX_VALUE;
     static double max = Double.MIN_VALUE;
 
-    static int sumAll = 0;
-
 
 
     public static void main(String[] args) throws Exception {
@@ -33,34 +31,26 @@ public class Main {
         }
 
         //가장 먼 두점 거리 최소
-        
+
         findMax(0,0);
-        if (m == 2)
-            System.out.println((int)(min*min));
-        else
-            System.out.println((int)Math.pow(calcSecond(),2));
+//        if (m == 2)
+//            System.out.println((int)(min*min));
+//        else
+            System.out.println((int)Math.pow(min,2));
     }
 
 
     static void findMax(int depth, int start) {
         if (depth == m) {
-            if (m==2){
+//            if (m==2){
 //                System.out.println(arrayList.get(0).x+" "+ arrayList.get(1).x+" ");
 //                System.out.println(dist(arrayList.get(0).x, arrayList.get(0).y,
 //                        arrayList.get(1).x, arrayList.get(1).y));
-                min = Math.min(min, dist(arrayList.get(0).x, arrayList.get(0).y,
-                        arrayList.get(1).x, arrayList.get(1).y));
-            }else{
+//                min = Math.min(min, dist(arrayList.get(0).x, arrayList.get(0).y,
+//                        arrayList.get(1).x, arrayList.get(1).y));
+//            }else{
                 double value = calcFirst();
-                if (max < value) {
-                    max = value;
-                    mList.clear();
-                    for (int i = 0; i < m; i++) {
-                        mList.add(arrayList.get(i));
-                    }
-                }
-            }
-
+                min = Math.min(min, value);
             return;
         }
 
@@ -85,20 +75,20 @@ public class Main {
 
         return maxVal;
     }
-    static double calcSecond() {
-        double minVal = Double.MAX_VALUE;
-
-        for (int i = 0; i < m; i++) {
-            for (int j = i+1; j < m; j++) {
-                pair p1 =mList.get(i);
-                pair p2 =mList.get(j);
-
-                minVal = Math.min(minVal,dist(p1.x, p1.y, p2.x, p2.y));
-            }
-        }
-
-        return minVal;
-    }
+//    static double calcSecond() {
+//        double minVal = Double.MAX_VALUE;
+//
+//        for (int i = 0; i < m; i++) {
+//            for (int j = i+1; j < m; j++) {
+//                pair p1 =mList.get(i);
+//                pair p2 =mList.get(j);
+//
+//                minVal = Math.min(minVal,dist(p1.x, p1.y, p2.x, p2.y));
+//            }
+//        }
+//
+//        return minVal;
+//    }
 
     static double dist(int x1, int y1, int x2, int y2) {
         return Math.sqrt((x1-x2)*(x1-x2) + (y1-y2)*(y1-y2));
@@ -114,8 +104,6 @@ public class Main {
         }
     }
         }
-
-
 
 
 
