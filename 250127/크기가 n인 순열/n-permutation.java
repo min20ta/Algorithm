1041c489ddac45c2b760cd2a5698521faa13a3de
__ <127599615+min20ta@users.noreply.github.com>
2025-01-27 21:56:32 +0900

    Title: 크기가 n인 순열, Time: 1103ms, Memory: 27MB, Status: Passed - CodeTree

---
+++ 250127/크기가 n인 순열/n-permutation.java
@@ -0,0 +1,52 @@
+
+import java.util.*;
+import java.io.*;
+
+
+public class Main {
+
+    static int n ;
+    static ArrayList<Integer> arrayList = new ArrayList<>();
+    static boolean [] visit;
+
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        visit = new boolean[n+1];
+
+        find(0);
+    }
+
+    static void find(int depth) {
+
+        if (depth == n){
+            for (int i = 0; i < n; i++) {
+                System.out.print(arrayList.get(i)+" ");
+            }
+            System.out.println();
+            return;
+        }
+
+        for (int i = 1; i <= n ; i++) {
+            if (!visit[i]) {
+                visit[i] = true;
+                arrayList.add(i);
+                find(depth+1);
+                arrayList.remove(arrayList.size()-1);
+                visit[i] = false;
+            }
+        }
+    }
+        }
+
+
+
+
+
+
+
+
+
+
