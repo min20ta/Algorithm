99e895bbacac07bd46c9c52fd7770dd68d959d40
__ <127599615+min20ta@users.noreply.github.com>
2025-01-27 20:05:07 +0900

    Title: n개의 점 중 m개 고르기, Time: 126ms, Memory: 9MB, Status: Passed - CodeTree

--- 250127/n개의 점 중 m개 고르기/choose-m-out-of-n-points.java
+++ 250127/n개의 점 중 m개 고르기/choose-m-out-of-n-points.java
@@ -12,7 +12,7 @@ public class Main {
     static ArrayList<pair> arrayList = new ArrayList<>();
     static ArrayList<pair> mList = new ArrayList<>();
     static double min = Double.MAX_VALUE;
-    static double max = Double.MIN_VALUE;
+    static int dist = 0;
 
 
 
@@ -33,22 +33,12 @@ public class Main {
         //가장 먼 두점 거리 최소
 
         findMax(0,0);
-//        if (m == 2)
-//            System.out.println((int)(min*min));
-//        else
-            System.out.println((int)Math.pow(min,2));
+        System.out.println(Math.round(Math.pow(min,2)));
     }
 
 
     static void findMax(int depth, int start) {
         if (depth == m) {
-//            if (m==2){
-//                System.out.println(arrayList.get(0).x+" "+ arrayList.get(1).x+" ");
-//                System.out.println(dist(arrayList.get(0).x, arrayList.get(0).y,
-//                        arrayList.get(1).x, arrayList.get(1).y));
-//                min = Math.min(min, dist(arrayList.get(0).x, arrayList.get(0).y,
-//                        arrayList.get(1).x, arrayList.get(1).y));
-//            }else{
                 double value = calcFirst();
                 min = Math.min(min, value);
             return;
@@ -75,20 +65,6 @@ public class Main {
 
         return maxVal;
     }
-//    static double calcSecond() {
-//        double minVal = Double.MAX_VALUE;
-//
-//        for (int i = 0; i < m; i++) {
-//            for (int j = i+1; j < m; j++) {
-//                pair p1 =mList.get(i);
-//                pair p2 =mList.get(j);
-//
-//                minVal = Math.min(minVal,dist(p1.x, p1.y, p2.x, p2.y));
-//            }
-//        }
-//
-//        return minVal;
-//    }
 
     static double dist(int x1, int y1, int x2, int y2) {
         return Math.sqrt((x1-x2)*(x1-x2) + (y1-y2)*(y1-y2));
@@ -104,6 +80,8 @@ public class Main {
         }
     }
         }
+
+
 
 
 
