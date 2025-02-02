5d25c6312352de43dabfd1ddf56f609ef6f63ad0
__ <127599615+min20ta@users.noreply.github.com>
2025-02-03 02:38:57 +0900

    Title: 트리 사촌, Time: 92ms, Memory: 10MB, Status: Passed - CodeTree

--- 250203/트리 사촌/beard-tree.java
+++ 250203/트리 사촌/beard-tree.java
@@ -14,11 +14,7 @@ public class Main {
     static ArrayList<Integer>[] tree;
     static int[] arr;
     static int[] parentArr;
-    static ArrayList<ArrayList<Integer>> arrayList = new ArrayList<>();
-    static int max = Integer.MIN_VALUE;
-    static int count;
-    static Map<Integer, Integer> map = new HashMap<>();
-    static Map<Integer, Integer> goMap = new HashMap<>();
+  
 
 
     public static void main(String[] args) throws Exception {
@@ -27,32 +23,30 @@ public class Main {
         n = Integer.parseInt(st.nextToken());
         k = Integer.parseInt(st.nextToken());
 
-
-        for (int i = 0; i < n + 1; i++) {
-            arrayList.add(new ArrayList<>());
-        }
-
-        arr = new int[n];
-        parentArr = new int[n];
-
-        for (int i = 0; i < n ; i++) {
-            st = new StringTokenizer(br.readLine());
+        arr = new int[n+1];
+        parentArr = new int[n+1];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= n ; i++) {
+            
             int a = Integer.parseInt(st.nextToken());
             arr[i] = a;
 
             if (a == k)
-                findingNode = a; //찾고자하는 노드의 인덱스
+                findingNode = i; //찾고자하는 노드의 인덱스
         }
 
-        int parent = -1;
-        for (int i = 1; i <n ; i++) {
+        int parent = 0;
+        for (int i = 2; i <=n ; i++) {
             if (arr[i-1]+1 < arr[i])
                 parent++;
 
             parentArr[i] = parent;
         }
 
-        for (int i = 0; i < n; i++) {
+        for (int i = 1; i <= n; i++) {
+            if(parentArr[parentArr[i]]==0 || parentArr[parentArr[findingNode]]== 0 )
+                continue;
+
             if (parentArr[i]!= parentArr[findingNode] 
                     && parentArr[parentArr[i]] == parentArr[parentArr[findingNode]])
                 ans++;
