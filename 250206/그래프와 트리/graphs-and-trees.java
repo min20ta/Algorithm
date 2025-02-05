b785ac8a3ce527326d5920d2bc51a48ca5487837
__ <127599615+min20ta@users.noreply.github.com>
2025-02-06 00:11:40 +0900

    Title: 그래프와 트리, Time: 129ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 250206/그래프와 트리/graphs-and-trees.java
@@ -0,0 +1,134 @@
+
+import java.util.*;
+import java.io.*;
+
+
+public class Main {
+
+    static int n;
+    static int m;
+    static int ans = Integer.MIN_VALUE;
+    static int root;
+    static boolean[] visit;
+    static int findingNode;
+    static boolean [] finish;
+    static int[][] lineArr;
+    static boolean cycle ;
+    static int deleteTwo = 0;
+    static ArrayList<ArrayList<Integer>> arrayList = new ArrayList<>();
+    static int subMax = Integer.MIN_VALUE;
+    static Set<Integer>set = new HashSet<>();
+
+
+
+
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+
+        for (int i = 0; i < n+1 ; i++) {
+            arrayList.add(new ArrayList<>());
+        }
+
+        int checkNode = 0;
+        for (int i = 0; i < m ; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            arrayList.get(a).add(b);
+            arrayList.get(b).add(a);
+
+            set.add(a); set.add(b);
+
+            checkNode = a;
+        }
+
+
+        finish = new boolean[n+1];
+        visit = new boolean[n+1];
+        visit[checkNode] = true;
+         boolean first = edge();
+         boolean second =  place(checkNode);
+
+         ans = n- set.size();
+//        System.out.println(first);
+//        System.out.println(second);
+//        System.out.println(cycle);
+         if (first || second || !cycle){
+             ans = 1 + n-set.size();
+         }else {
+             ans = 0;
+         }
+
+
+
+
+        System.out.println(ans);
+
+    }
+
+
+
+
+    static boolean edge() {
+        if (set.size() -1 == m)
+            return true;
+        return false;
+    }
+
+
+    static boolean place(int node) {
+
+          tree(node);
+
+          boolean flag = true;
+
+          for (Integer i : set) {
+              if (!visit[i]){
+                  flag = false;
+                  break;
+              }
+          }
+
+          if (flag)
+              return true;
+          else
+              return false;
+    }
+
+    static void tree(int node) {
+        for (Integer next : arrayList.get(node)) {
+            if (!visit[next]) {
+                visit[next] = true;
+                tree(next);
+            }else if (!finish[next])
+                cycle = true;
+        }
+        finish[node] = true;
+    }
+
+
+    static class pair {
+        int x;
+        int len;
+
+        pair(int x, int len) {
+            this.x = x;
+            this.len = len;
+        }
+    }
+
+
+
+
+
+
+
+}
+
+
