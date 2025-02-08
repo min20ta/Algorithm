57954820a9f68bb825a94ef96ac877c65973f460
__ <127599615+min20ta@users.noreply.github.com>
2025-02-08 19:59:31 +0900

    Title: 트리에서 공 떨어뜨리기, Time: 263ms, Memory: 23MB, Status: Wrong Answer - CodeTree

---
+++ 250208/트리에서 공 떨어뜨리기/drop-a-ball-in-a-tree.java
@@ -0,0 +1,90 @@
+
+import java.util.*;
+import java.io.*;
+
+
+public class Main {
+
+    static int n;
+    static long k;
+    static int idx = 0;
+    static int root;
+    static boolean[] visit;
+    static int edgeCount = 0;
+    static int nodeCount = 0;
+    static boolean [] finish;
+    static int [] right;
+    static int [] left;
+    static int [] arr;
+    static boolean cycle = false ;
+    static int deleteTwo = 0;
+    static ArrayList<ArrayList<Integer>> arrayList = new ArrayList<>();
+    static int subMax = Integer.MIN_VALUE;
+    static Set<Integer>set = new HashSet<>();
+
+
+
+
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+
+        left = new int[2*n+1];
+        right = new int[2*n+1];
+
+        for (int i = 1; i <= n ; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            left[i] = x;
+            right[i] = y;
+        }
+
+
+        k = Long.parseLong(br.readLine());
+        //값이 클때는 시뮬레이션 불가능
+
+        int node = 1;
+        int idx = 1;
+        int ans = 0;
+
+        while (k>0) {
+
+            if (left[node] == -1 && right[node] == -1){
+                ans = node;
+                break;
+            }
+
+            if (k % 2 != 0){ //홀수일때 -> 왼쪽 서브트리
+                if (left[node] == -1 && right[node] > 0)
+                    node = right[node];
+                else
+                    node = left[node];
+            }else {
+                if (left[node] > 0 && right[node] == -1)
+                    node = left[node];
+                else
+                    node = right[node];
+            }
+            if (k%2 == 0)
+                k /= 2;
+            else 
+                k /= 2 +1;
+
+        }
+        System.out.println(ans);
+
+
+    }
+
+
+
+
+
+
+
+}
+
+
