d7a0264adc30433f10ed45086ebaa1e8be6a313c
__ <127599615+min20ta@users.noreply.github.com>
2025-02-14 22:02:25 +0900

    Title: 떨어지는 1자 블록, Time: 90ms, Memory: 9MB, Status: Wrong Answer - CodeTree

---
+++ 250214/떨어지는 1자 블록/falling-horizontal-block.java
@@ -0,0 +1,100 @@
+import java.util.*;
+import java.io.*;
+
+
+public class Main {
+    static int[][] map;
+    static int[][] grid;
+    static int[][] temp;
+    static int endOfArray;
+    static int idx = 0;
+    static int[] dx = {0, 1, 0, 0,};
+    static int[] dy = {0, 0, -1, 1};
+    static int n;
+    static int m;
+    static int k;
+
+    static int endIndex;
+    static int max = Integer.MIN_VALUE;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        k = Integer.parseInt(st.nextToken());
+        k--;
+
+
+        map = new int[n][n];
+
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < n; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        block();
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(map[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+
+    }
+
+    static void block() {
+        int start = k; //1열부터 3열
+        int end = k + m - 1;
+
+        for (int i = end; i >= start; i--) {
+            int count = 0;
+            int startIdx = 0;
+            for (int j = 0; j < n; j++) {
+                if (i == 0 && map[i][j] == 0)
+                    count++;
+                else if (map[i][j] == 0 && map[i - 1][j] == 0) {
+                    count++;
+                } else {
+                    if (count >= m) {
+                        //System.out.println(count+" i: "+i);
+                        if (check(startIdx, i, j)) {
+                            for (int l = startIdx; l < startIdx + m; l++) {
+                                map[i][l] = 1;
+                            }
+                            return;
+                        }
+                    }
+                    startIdx = j+1;
+                    count = 0;
+                }
+            }
+        }
+    }
+
+
+    static boolean check(int startIdx, int row, int endCol) {
+        for (int i = startIdx; i < endCol; i++) {
+            int x = row;
+            int y = startIdx;
+
+            for (int j = 0; j < 4; j++) {
+                int nx = x + dx[j];
+                int ny = y + dy[j];
+
+                if (nx >= 0 && nx < n && ny >= 0 && ny < n) {
+                    if (map[nx][ny] == 1) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+
+    }
+}
+
