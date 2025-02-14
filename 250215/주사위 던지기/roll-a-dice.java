35d317fcca7bc6bfde01a417a7e364cddc0e3ee2
__ <127599615+min20ta@users.noreply.github.com>
2025-02-15 02:56:45 +0900

    Title: 주사위 던지기, Time: 101ms, Memory: 9MB, Status: Wrong Answer - CodeTree

---
+++ 250215/주사위 던지기/roll-a-dice.java
@@ -0,0 +1,131 @@
+import java.util.*;
+import java.io.*;
+
+
+public class Main {
+    static int[][] map;
+    static int[][] dice = {{0,5,0},{4,6,3},{0,2,0},{0,1,0}};
+    static int[][] temp;
+    static int endOfArray;
+    static int idx = 0;
+    static int[] dx = {0, 1, 0, -1,};
+    static int[] dy = {-1, 0, 1, 0};
+    static char [] dir ;
+    static int [] left = {6,4,1,3};
+    static int [] right = {6,3,1,4};
+    static int [] down = {6,2,1,5};
+    static int [] up = {6,5,1,2};
+    static int n;
+    static int m;
+    static int r;
+    static int c;
+    static int x;
+    static int y;
+    static int index =0;
+
+    static int endIndex;
+    static int max = Integer.MIN_VALUE;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        r = Integer.parseInt(st.nextToken()); r--;
+        c = Integer.parseInt(st.nextToken()); c--;
+
+
+
+        map = new int[n][n];
+        dir = new char[m];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < m; i++) {
+            dir[i] = st.nextToken().charAt(0);
+        }
+
+
+
+        int direction = 0;
+        map[r][c] = 6;
+        x = r; y = c;
+
+        for (int i = 0; i < m; i++) {
+            char c = dir[i];
+
+            if (c == 'L') direction = 0;
+            else if (c == 'D') direction = 1;
+            else if (c == 'R') direction = 2;
+            else if (c == 'U') direction = 3;
+
+            move(direction);
+            changeDice(direction);
+        }
+
+
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+              //  System.out.print(map[i][j]+" ");
+                sum += map[i][j];
+            }
+          //  System.out.println();
+        }
+
+        System.out.println(sum);
+
+
+
+
+
+
+
+    }
+
+    static void move(int direction) {
+
+        int curX = 1; int curY = 1;
+        int diceX = curX + dx[direction];
+        int diceY = curY + dy[direction];
+
+        int nx = x + dx[direction];
+        int ny = y + dy[direction];
+
+        if (nx >= 0 && nx < n && ny >= 0 && ny < n) {
+            map[nx][ny] = dice[diceX][diceY];
+            x = nx; y = ny;
+        }
+    }
+
+    static void changeDice(int direction) {
+        if (direction == 0) {
+            int temp = dice[1][2];
+            int temp2 = dice[3][1];
+            for (int i = 2; i >= 1; i--) {
+                dice[1][i] = dice[1][i-1];
+            }
+            dice[1][0] = temp2;
+            dice[3][1] = temp;
+        }else if (direction == 1) { //down
+            int temp = dice[0][1];
+            for (int i = 0; i < 3; i++) {
+                dice[i][1] = dice[i+1][1];
+            }
+            dice[3][1] = temp;
+        }else if (direction == 2) { //right
+            int temp = dice[1][0];
+            int temp2 = dice[3][1];
+            for (int i = 0; i <= 1; i++) {
+                dice[1][i] = dice[1][i+1];
+            }
+            dice[1][2] = temp2;
+            dice[3][1] = temp;
+        }else { //uphhn
+            int temp = dice[3][1];
+            for (int i = 1; i <= 3; i++) {
+                dice[i][1] = dice[i-1][1];
+            }
+            dice[0][1] = temp;
+        }
+    }
+}
+
