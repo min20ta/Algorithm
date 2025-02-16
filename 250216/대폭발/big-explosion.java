f34fdaed27c87a4661a4e4e94634c0086f91169a
__ <127599615+min20ta@users.noreply.github.com>
2025-02-16 21:51:24 +0900

    Title: 대폭발, Time: 92ms, Memory: 9MB, Status: Passed - CodeTree

--- 250216/대폭발/big-explosion.java
+++ 250216/대폭발/big-explosion.java
@@ -16,6 +16,8 @@ public class Main {
     static int r;
     static int c;
     static Queue<pair> q = new LinkedList<>();
+    static ArrayList<pair> list = new ArrayList<>();
+    static ArrayList<pair> list2 = new ArrayList<>();
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -39,39 +41,64 @@ public class Main {
     static void bomb(int r, int c) {
 
         map[r][c] = 1;
-        q.add(new pair(r,c,1));
+//        q.add(new pair(r,c,1));
+        list.add(new pair(r,c,1));
 
-        while(!q.isEmpty()) {
+//        while(!q.isEmpty()) {
+//
+//            pair p  = q.poll();
+//            if (p.t == m+1)
+//              continue;
+//            int x = p.x;
+//            int y = p.y;
+//            int t = p.t;
+//
+//            for (int i = 0; i < 4; i++) {
+//                int nx = x + dx[i]*(1<<(t-1));
+//                int ny = y + dy[i]*(1<<(t-1));
+//                if (nx >= 0 && nx < n && ny >= 0 && ny < n) {
+//                    map[nx][ny] = 1;
+//                    //System.out.println(nx +" "+ ny);
+//                }
+//            }
+//
+//            addQ(t+1);
+//        }
 
-            pair p  = q.poll();
-            if (p.t == m+1)
-              break;
-            int x = p.x;
-            int y = p.y;
-            int t = p.t;
+        loop:
+        while(true) {
 
-            for (int i = 0; i < 4; i++) {
-                int nx = x + dx[i]*(1<<(t-1));
-                int ny = y + dy[i]*(1<<(t-1));
-                if (nx >= 0 && nx < n && ny >= 0 && ny < n) {
-                    map[nx][ny] = 1;
+            int size = list.size();
+
+            for (int k = 0; k < size ; k++) {
+                pair p = list.get(k);
+
+                if (p.t == m+1)
+                    break loop;
+
+                int x = p.x;
+                int y = p.y;
+                int t = p.t;
+                list2.add(new pair(x,y,t+1));
+
+                for (int i = 0; i < 4; i++) {
+                    int nx = x + dx[i]*(1<<(t-1));
+                    int ny = y + dy[i]*(1<<(t-1));
+                    if (nx >= 0 && nx < n && ny >= 0 && ny < n) {
+                        map[nx][ny] = 1;
+                       // System.out.println(nx+" "+ny);
+                        list2.add(new pair(nx, ny, t+1));
+                    }
                 }
             }
 
-            addQ(t+1);
+            list = new ArrayList<>(list2);
+            list2.clear();
         }
         }
 
-        static void addQ(int t) {
-            for (int i = 0; i < n; i++) {
-                for (int j = 0; j < n; j++) {
-                    if (map[i][j] == 1)
-                        q.add(new pair(i,j,t));
-
-                }
-            }
-        }
-        static int count(){
+        
+    static int count(){
 
         int sum = 0;
             for (int i = 0; i < n; i++) {
