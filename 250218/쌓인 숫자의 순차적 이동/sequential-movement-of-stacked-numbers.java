df94bacf4b78a4b248b44dac5c56ba12e9e612dc
__ <127599615+min20ta@users.noreply.github.com>
2025-02-18 23:03:30 +0900

    Title: 쌓인 숫자의 순차적 이동, Time: 119ms, Memory: 9MB, Status: Passed - CodeTree

--- 250218/쌓인 숫자의 순차적 이동/sequential-movement-of-stacked-numbers.java
+++ 250218/쌓인 숫자의 순차적 이동/sequential-movement-of-stacked-numbers.java
@@ -9,8 +9,8 @@ public class Main {
     static int[][] dir;
     static int[][] dir2;
     static int idx = 0;
-    static int[] dx = {-1,0,1,1,1,0,-1,-1};
-    static int[] dy = {-1,-1,-1,0,1,1,1,0};
+    static int[] dx = {-1, 0, 1, 1, 1, 0, -1, -1};
+    static int[] dy = {-1, -1, -1, 0, 1, 1, 1, 0};
     static int n;
     static int m;
     static int t;
@@ -18,9 +18,9 @@ public class Main {
     static int y = 0;
     static int next;
     static Queue<pair> q = new LinkedList<>();
-    static ArrayList<Integer> list = new ArrayList<>();
+    static ArrayList<Integer>[][] list;
     static ArrayList<pair> list2 = new ArrayList<>();
-    static ArrayList<int [][]> dirList = new ArrayList<>();
+    static ArrayList<int[][]> dirList = new ArrayList<>();
     static int max = Integer.MIN_VALUE;
 
     public static void main(String[] args) throws Exception {
@@ -29,15 +29,18 @@ public class Main {
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
 
-        map= new int[n][n];
-        display = new pair[n*n+1];
         num = new int[m];
+        list = new ArrayList[n + 1][n + 1];
 
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                list[i][j] = new ArrayList<>();
+            }
+        }
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < n; j++) {
-                map[i][j] = Integer.parseInt(st.nextToken()); //쓸모가 없을듯?
-                display[map[i][j]] = new pair(i,j,0,map[i][j]);
+                list[i][j].add(Integer.parseInt(st.nextToken()));
             }
         }
 
@@ -47,35 +50,35 @@ public class Main {
         }
 
         for (int i = 0; i < m; i++) {
-            findMax(display[num[i]].x,display[num[i]].y, num[i]);
+            findMax(num[i]);
         }
 
-        for (int i = 0; i < n ; i++) {
-            for (int j = 0; j < n; j++) {
-                printAns(i,j);
-            }
-        }
+
+        printAns();
+
 
     }
 
-    static void findMax(int x, int y, int num) {
+    static void findMax(int num) {
+
+        pair curPosition = getPosition(num);
 
         int max = Integer.MIN_VALUE;
-        int maxX = 0; int maxY = 0;
-        int nx = 0; int ny = 0;
+        int maxX = 0;
+        int maxY = 0;
+        int nx = 0;
+        int ny = 0;
 
         for (int i = 0; i < 8; i++) {
-            list.clear();
-            nx = x + dx[i];
-            ny = y + dy[i];
+            nx = curPosition.x + dx[i];
+            ny = curPosition.y + dy[i];
 
             if (nx >= 0 && nx < n && ny >= 0 && ny < n) {
-
-                getNumbers(nx,ny);
-                for (int j = 0; j < list.size(); j++) {
-                    if (list.get(j) > max){
-                        max = list.get(j);
-                        maxX = nx; maxY = ny;
+                for (int j = 0; j < list[nx][ny].size(); j++) {
+                    if (list[nx][ny].get(j) > max) {
+                        max = list[nx][ny].get(j);
+                        maxX = nx;
+                        maxY = ny;
                     }
                 }
             }
@@ -83,86 +86,71 @@ public class Main {
         //System.out.println(max + " "+ maxX+" "+maxY);
         //최대값 자리로 이동
         //내 위 애들 찾아서
-        getUp(num);
-        //이동
-        list.clear();
-        getNumbers(maxX,maxY);
-        int curNum = list.size()-1;
-        int nextNum = list2.size(); //갖고가는거
-    //    System.out.println("cur: "+ curNum);
-      //  System.out.println("next: "+ nextNum);
-        if (curNum+1 != 0) {
-            for (int i = list2.size() - 1; i >= 0; i--) {
-                display[list2.get(i).num] = new pair(maxX, maxY, nextNum-- + curNum, list2.get(i).num);
-            }
-            list2.clear();
-        }
-//        for (int i = 1; i <= n*n; i++) {
-//            System.out.println(i+": "+display[i].x+" "+display[i].y +" "+ display[i].howManyBack);
-//        }
+        if(max != Integer.MIN_VALUE)
+            move(curPosition, maxX, maxY, num);
 
     }
-    static void getNumbers(int x, int y) {
-        for (int i = 1; i <= n*n  ; i++) {
-            if (display[i].x == x && display[i].y == y)
-                list.add(i);
-        }
-    }
 
-    static void getUp(int num) { //상위 같이 움직이기
-        int x = display[num].x;
-        int y = display[num].y;
-        int howManyBack = display[num].howManyBack;
-        list2.add(display[num]);
-
-        for (int i = 1 ; i <= n*n ; i++) {
-            if (i != max && display[i].x == x && display[i].y == y){
-                if (display[i].howManyBack > howManyBack) {
-                    list2.add(display[i]);
+    static pair getPosition(int num) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                for (int k = 0; k < list[i][j].size(); k++) {
+                    if (list[i][j].get(k) == num)
+                        return new pair(i, j);
                 }
             }
         }
-        Collections.sort(list2);
+        return new pair(0, 0);
+    }
+
+    static void move(pair cur, int nx, int ny, int num) {
+        int x = cur.x;
+        int y = cur.y;
+        boolean canMove = false;
+
+        for (int i = 0; i < list[x][y].size(); i++) {
+            if (list[x][y].get(i) == num) {
+                canMove = true;
+            }
+
+            if (canMove)
+                list[nx][ny].add(list[x][y].get(i));
+        }
+
+        //원래자리 삭제
+        while (list[x][y].get(list[x][y].size() - 1) != num) {
+            list[x][y].remove(list[x][y].size() - 1);
+        }
+        list[x][y].remove(list[x][y].size() - 1);
 
     }
 
-    static void printAns(int x, int y) {
-        List<pair> temp = new ArrayList<>();
-        for (int i = 1; i <= n*n; i++) {
-            if (display[i].x == x && display[i].y == y)
-                temp.add(display[i]);
-        }
-        if(temp.size() == 0){
-            System.out.println("None");
-            return;
-        }
 
-        Collections.sort(temp);
-        for (int i = temp.size()-1; i >= 0 ; i--) {
-            System.out.print(temp.get(i).num+" ");
+    static void printAns() {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (list[i][j].size() == 0)
+                    System.out.println("None");
+                else {
+                    for (int k = list[i][j].size() - 1; k >= 0; k--) {
+                        System.out.print(list[i][j].get(k) + " ");
+                    }
+                    System.out.println();
+                }
+            }
         }
-        System.out.println();
     }
 
 
-    static class pair implements Comparable<pair>{
+    static class pair {
         int x;
         int y;
-        int howManyBack;
-        int num;
 
-
-        pair(int x, int y, int howManyBack, int num) {
-            this.x =x;
+        pair(int x, int y) {
+            this.x = x;
             this.y = y;
-            this.howManyBack = howManyBack;
-            this.num = num;
         }
-
-        @Override
-        public int compareTo(pair p) {
-            return this.howManyBack - p.howManyBack;
-        }
-        }
+    }
 }
+
 
