1509a37cba59b9e920efc9079c9d8865c8d53612
__ <127599615+min20ta@users.noreply.github.com>
2025-02-26 00:43:24 +0900

    Title: 최소 정수 출력, Time: 567ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 250226/최소 정수 출력/min-integer-output.java
@@ -0,0 +1,43 @@
+import java.util.*;
+import java.io.*;
+
+
+public class Main {
+
+    static int[][] dir;
+    static int[][] dir2;
+    static int time = 0;
+    static int[] dx = {-1, 0, 0, 1};
+    static int[] dy = {0, -1, 1, 0};
+    static int n;
+    static int m;
+    static int t;
+    static int lastTime = 0 ;
+    static int ansSum;
+    static int ansW;
+    static int x = 0;
+    static int y = 0;
+
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+        for (int i = 0; i < n; i++) {
+            int num = Integer.parseInt(br.readLine());
+            if (num > 0)
+                pq.add(num);
+            else{
+                if (pq.isEmpty())
+                    System.out.println(0);
+                else
+                    System.out.println(pq.poll());
+        }}
+
+
+    }
+}
+
+
