abbe4ac925d1425f769b9075b79ee8f30457dd0e
__ <127599615+min20ta@users.noreply.github.com>
2025-02-26 19:17:56 +0900

    Title: 앞에서부터 삭제하기 2, Time: 3078ms, Memory: 152MB, Status: Time Limit Exceed - CodeTree

--- 250226/앞에서부터 삭제하기 2/delete-it-from-the-beginning-2.java
+++ 250226/앞에서부터 삭제하기 2/delete-it-from-the-beginning-2.java
@@ -20,44 +20,42 @@ public class Main {
         n = Integer.parseInt(st.nextToken());
 
         PriorityQueue<Integer> pq = new PriorityQueue<>();
+        PriorityQueue<Integer> copyPQ = new PriorityQueue<>();
         ArrayList<Integer> list = new ArrayList<>();
 
         // k개 삭제후, 남아있는 것중가장작은 거 하나 제외한 평균.
         // 평균 최대
 
         st = new StringTokenizer(br.readLine());
+        int sum = 0; int copySum =0;
         for (int i = 0; i < n; i++) {
             int x = Integer.parseInt(st.nextToken());
             list.add(x);
+            pq.add(x);
+            sum += x;
         }
 
 
         //완탐
         //10^5 * 5
         for (int k = 1; k <= n-2 ; k++) {
-            for (int i = k; i < n; i++) {
-                pq.add(list.get(i));
+            copySum = sum;
+            copyPQ = new PriorityQueue<>(pq);
+
+            for (int i = 0; i < k; i++) {
+                copySum -= list.get(i);
+                copyPQ.remove(i);
             }
 
-            int remove = pq.poll();
-            int sum = 0; int count = 0;
-            for (int i = k; i < n; i++) {
-                if (remove == list.get(i) && count == 0) {
-                    count++;
-                    continue;
-                }
-                sum += list.get(i);
-            }
-            double avg = (double)sum/(n-k-1);
+            copySum -= copyPQ.poll();
+            double avg = (double)copySum/(n-k-1);
             max = Math.max(max, avg);
 
-            pq.clear();
         }
 
         System.out.printf("%.2f\n", max);
 
     }
 }
-
 
 
