f199426605766ea370be6a03d60d93285826073a
__ <127599615+min20ta@users.noreply.github.com>
2025-03-09 01:25:54 +0900

    Title: 공백을 기준으로 출력, Time: 86ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 250309/공백을 기준으로 출력/output-based-on-space.java
@@ -0,0 +1,69 @@
+
+import java.util.*;
+import java.io.*;
+
+
+public class Main {
+
+    static int[][] dir;
+    static int[][] dir2;
+    static int time = 0;
+    static int[] dx = {0,1,0,-1};
+    static int[] dy = {1, 0, -1, 0};
+    static int n;
+    static int m;
+    static int k;
+    static int q;
+    static int max = Integer.MIN_VALUE;
+
+
+    static int [][] arr;
+    static int [][] dp;
+
+    static int [] a;
+    static int [] b;
+    static TreeMap<Integer,Integer> map = new TreeMap<>();
+    static TreeSet<Integer> nums = new TreeSet<>();
+
+
+
+    static final int MAX_NUM = 100000;
+
+
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        //StringTokenizer st = new StringTokenizer(br.readLine());
+       String str = br.readLine();
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == ' ')
+                continue;
+            System.out.print(str.charAt(i));
+        }
+       str = br.readLine();
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == ' ')
+                continue;
+            System.out.print(str.charAt(i));
+        }
+
+
+
+
+    }
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
