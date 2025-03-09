41ec529f41b5cbcb26d242ffa0c5c3d6b5b55450
__ <127599615+min20ta@users.noreply.github.com>
2025-03-09 21:13:08 +0900

    Title: 두 번째를 첫 번째로, Time: 84ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 250309/두 번째를 첫 번째로/second-to-first.java
@@ -0,0 +1,67 @@
+
+import java.util.*;
+import java.io.*;
+
+
+public class Main {
+
+    static int[][] dir;
+    static int[][] dir2;
+    static int time = 0;
+    static int[] dx = {0,1,0,-1};
+    static int[] dy = {1, 0, -1, 0};
+    static int n;
+    static int m;
+    static int k;
+    static int q;
+    static int max = Integer.MIN_VALUE;
+
+
+    static int [][] arr;
+    static int [][] dp;
+
+    static int [] a;
+    static int [] b;
+    static TreeMap<Integer,Integer> map = new TreeMap<>();
+    static TreeSet<Integer> nums = new TreeSet<>();
+
+
+
+    static final int MAX_NUM = 100000;
+
+
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        //StringTokenizer st = new StringTokenizer(br.readLine());
+       String str = br.readLine();
+       char [] arr = str.toCharArray();
+       char second = arr[1];
+       char first = arr[0];
+
+       StringBuilder sb = new StringBuilder(str);
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == second)
+                sb.setCharAt(i,first);
+        }
+
+        System.out.println(sb.toString());
+
+
+    }
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
