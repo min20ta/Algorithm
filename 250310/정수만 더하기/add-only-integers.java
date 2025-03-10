603ef31a37688b22d06e82a9fee98382a0d191c5
__ <127599615+min20ta@users.noreply.github.com>
2025-03-10 16:22:32 +0900

    Title: 정수만 더하기, Time: 90ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 250310/정수만 더하기/add-only-integers.java
@@ -0,0 +1,47 @@
+
+import java.util.*;
+import java.io.*;
+
+
+public class Main {
+
+    static int[][] dir;
+    static int[][] dir2;
+    static int time = 0;
+    static int[] dx = {0, 1, 0, -1};
+    static int[] dy = {1, 0, -1, 0};
+    static int n;
+    static int m;
+    static int k;
+    static int q;
+    static int max = Integer.MIN_VALUE;
+
+
+    static int[][] arr;
+    static int[][] dp;
+
+    static int[] a;
+    static int[] b;
+    static TreeMap<Integer, Integer> map = new TreeMap<>();
+    static TreeSet<Integer> nums = new TreeSet<>();
+
+
+    static final int MAX_NUM = 100000;
+
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        // StringTokenizer st = new StringTokenizer(br.readLine());
+        String str = br.readLine();
+        int sum = 0;
+
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+           if ('0' <= c && c <= '9') {
+                sum += Integer.parseInt(c + "");
+            }
+        }
+
+        System.out.println(sum);
+    }
+}
