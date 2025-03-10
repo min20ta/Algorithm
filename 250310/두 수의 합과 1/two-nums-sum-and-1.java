d83a84fd6feb19fc0dbf36973849e3911c65a2c5
__ <127599615+min20ta@users.noreply.github.com>
2025-03-10 16:55:39 +0900

    Title: 두 수의 합과 1, Time: 92ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 250310/두 수의 합과 1/two-nums-sum-and-1.java
@@ -0,0 +1,48 @@
+
+import java.util.*;
+import java.io.*;
+
+
+public class Main {
+
+    static int[][] dir;
+    static int[][] dir2;
+    static int time = 0;
+    static int[] dx = {0, 1, 0, -1};
+    static int[] dy = {1, 0, -1, 0};
+    static int n;
+    static int m;
+    static int k;
+    static int q;
+    static int max = Integer.MIN_VALUE;
+
+
+    static int[][] arr;
+    static int[][] dp;
+
+    static int[] a;
+    static int[] b;
+    static TreeMap<Integer, Integer> map = new TreeMap<>();
+    static TreeSet<Integer> nums = new TreeSet<>();
+
+
+    static final int MAX_NUM = 100000;
+
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int str = Integer.parseInt(st.nextToken());
+        int str2 = Integer.parseInt(st.nextToken());
+        int sum = str +str2;
+        String s = Integer.toString(sum);
+        int count = 0;
+        for (int i = 0; i < s.length() ; i++) {
+            if (s.charAt(i) == '1')
+                count++;
+        }
+
+        System.out.println(count);
+    }
+}
