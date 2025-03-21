d38773a8f9818e58e358cd985a0f5361861d32bd
__ <127599615+min20ta@users.noreply.github.com>
2025-03-21 16:23:07 +0900

    Title: 빙하, Time: 203ms, Memory: 16MB, Status: Passed - CodeTree

--- 250321/빙하/glacier.java
+++ 250321/빙하/glacier.java
@@ -26,6 +26,7 @@ public class Main {
     static int min = Integer.MAX_VALUE;
     static List<Integer> list = new ArrayList<>();
     static Queue<pair> q = new LinkedList<>();
+    static Queue<pair> nextQ = new LinkedList<>();
     static int meltCounts = 0;
     static int t = 0;
 
@@ -37,6 +38,7 @@ public class Main {
         m = Integer.parseInt(st.nextToken());
 
         arr = new int[n][m];
+        visit= new boolean[n][m];
 
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
@@ -48,28 +50,22 @@ public class Main {
             //바깥 = 0 물 1= 빙하
             // 1초마다 물닿은 빙하 녹음. but 물의 상하좌우 빙하면 못녹임
             //답: 빙하전체 녹는 시간, 마지막 녹는 크기
-            visit = new boolean[n][m];
-            for (int j = 0; j < m; j++) {
-                visit[0][j] = true;
-                visit[n-1][j] = true;
-            }
-            for (int j = 0; j < n; j++) {
-                visit[j][0] = true;
-                visit[j][m-1] = true;
-            }
 
-            int x = 1; int y = 1;
+            q.add(new pair(0,0));
+            visit[0][0] = true;
 
-            while(!checkAllMelt()) {
+            while(true) {
                 meltCounts = 0;
-                t++;
-                outSideMelt(x++,y++);
-                //System.out.println("1: "+ meltCounts);
+                nextQ.clear();
                 bfs();
-                //System.out.println("2: "+ meltCounts);
-                list.add(meltCounts);
-
+                if (meltCounts == 0)
+                    break;
+                else{
+                    t++;
+                    list.add(meltCounts);
+                }
             }
+
 
 
             System.out.println(t + " "+ list.get(list.size()-1));
@@ -79,54 +75,7 @@ public class Main {
 
 
     }
-    static boolean checkAllMelt() {
-        int allMeltCounts = 0;
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (arr[i][j] == 0)
-                    allMeltCounts++;
-            }
-        }
-        if (allMeltCounts == n*m)
-            return true;
-        else
-            return false;
-    }
-
-    static void outSideMelt(int x, int y) {
-        y--;
-        int row = x; int col = y+1;
-        for (int i = 0; i < 4; i++) {
-
-            while (true) {
-                int nx = x + dx[i];
-                int ny = y + dy[i];
-
-                if (!(nx >= row && nx < n-row && ny >= col && ny < m-col)) {
-//                    if (i == 0) {x = row; y = m-1-col;}
-//                    if (i == 1) {x = n-1-row; y = m-1-col;}
-//                    if (i == 2) {x = n-1-row; y = col;}
-                    break;
-                }
-
-                if (!visit[nx][ny]) {
-                    visit[nx][ny] = true;
-                    if (arr[nx][ny] == 0)
-                        q.add(new pair(nx, ny));
-                    else {
-                        arr[nx][ny] = 0;
-                        meltCounts++;
-                        //System.out.println(meltCounts);
-                      //  System.out.println(i+" "+nx+" "+ny);
-                    }
-                }
-                x = nx; y = ny;
-            }
-        }
-
-
-    }
     static void bfs() {
 
         while (!q.isEmpty()) {
@@ -142,9 +91,10 @@ public class Main {
                 if (nx >= 0 && nx < n && ny >=0 && ny < m) {
                     if (!visit[nx][ny]) {
                         visit[nx][ny] = true;
-                        if (arr[nx][ny] == 0){
+                        if (arr[nx][ny] == 0){ //물을 큐에 추가
                             q.add(new pair(nx, ny));
-                        }else {
+                        }else { //녹은 빙하는 다음 번 큐 대상
+                            nextQ.add(new pair(nx,ny));
                             arr[nx][ny] = 0;
                             meltCounts++;
                         }
@@ -152,6 +102,7 @@ public class Main {
                 }
             }
         }
+        q = new LinkedList<>(nextQ);
     }
 
   static class pair{
