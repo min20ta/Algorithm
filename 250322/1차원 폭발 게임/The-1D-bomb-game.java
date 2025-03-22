588563ba713cf90988f1e75a6c6326862b310799
__ <127599615+min20ta@users.noreply.github.com>
2025-03-22 20:20:42 +0900

    Title: 1차원 폭발 게임, Time: 90ms, Memory: 9MB, Status: Wrong Answer - CodeTree

---
+++ 250322/1차원 폭발 게임/The-1D-bomb-game.java
@@ -0,0 +1,88 @@
+
+import java.util.*;
+import java.io.*;
+
+
+public class Main {
+
+    static int[][] arr;
+    static int[][] copyArr;
+    static boolean[] visit;
+    static int time = 0;
+    static int[] dx = {0, 1, 0, -1};
+    static int[] dy = {1, 0, -1, 0};
+    static int n;
+    static int m;
+    static int q;
+
+    static int max = Integer.MIN_VALUE;
+
+    static int min = Integer.MAX_VALUE;
+    static List<Integer> list = new ArrayList<>();
+
+
+
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+
+        //1~100 n개 폭탄
+        //m 개 이상 연속으로 같은 숫자 폭탄 터짐. 아래로 내려옴
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            list.add(Integer.parseInt(st.nextToken()));
+        }
+
+        while (true) {
+            int count = 1;
+            boolean isBomb = false;
+            for (int i = 1; i < list.size(); i++) {
+                if (list.get(i) == list.get(i - 1)) {
+                    count++;
+                    isBomb = true;
+                } else {
+                    if (count >= m) {
+                        for (int j = i - 1; j >= i - count; j--) {
+                            list.remove(j);
+                        }
+                    }
+                    count = 1;
+                }
+
+            }
+
+            if (count >= m) {
+                int size = list.size();
+                for (int j = size-1; j >= size - count; j--) {
+                    list.remove(j);
+                }
+            }
+
+            if (!isBomb)
+                break;
+        }
+
+        System.out.println(list.size());
+        for (int a : list)
+            System.out.println(a);
+
+
+
+
+    }
+
+  static class pair{
+        int x;
+        int y;
+
+        pair(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+  }
+}
