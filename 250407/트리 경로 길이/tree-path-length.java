af95bf21ccb1242a5be89223752c721a6b20d71e
__ <127599615+min20ta@users.noreply.github.com>
2025-04-07 18:39:04 +0900

    Title: 트리 경로 길이, Time: 1673ms, Memory: 60MB, Status: Passed - Codetree

--- 250407/트리 경로 길이/tree-path-length.java
+++ 250407/트리 경로 길이/tree-path-length.java
@@ -5,13 +5,22 @@ import java.io.*;
 
 public class Main {
 
-    static int[] parent;
+    static ArrayList<Integer>[][] arr ;
+    static int[][] parent;
     static int[] depth ;
     static boolean[] visit;
+    static int time = 0;
+    static int[] dx = {0, 1, -1, 0,-1,-1,1,1};
+    static int[] dy = {-1, 0,0, 1,-1,1,1,-1};
     static int n;
+    static int m;
     static int q;
-    
+    static int x;
+    static int y;
 
+    static int max = Integer.MIN_VALUE;
+
+    static int min = Integer.MAX_VALUE;
     static ArrayList<ArrayList<Integer>> tree = new ArrayList<>();
 
 
@@ -20,7 +29,7 @@ public class Main {
         StringTokenizer st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
 
-        parent = new int[n+1];
+        parent = new int[18][n+1];
         visit = new boolean[n+1];
         depth = new int[n+1];
 
@@ -39,6 +48,12 @@ public class Main {
         depth[1] = 1;
         visit[1] = true;
         tree(1);
+
+        for (int h = 1; h <= 17 ; h++) {
+            for (int i = 1; i <= n ; i++) {
+                parent[h][i] = parent[h-1][parent[h-1][i]];
+            }
+        }
 
         q = Integer.parseInt(br.readLine());
         for (int i = 0; i < q; i++) {
@@ -59,7 +74,7 @@ public class Main {
         for(int next: tree.get(node)) {
             if (!visit[next]){
                 visit[next] = true;
-                parent[next] = node;
+                parent[0][next] = node;
                 depth[next] = depth[node]+1;
                 tree(next);
             }
@@ -67,23 +82,35 @@ public class Main {
     }
 
 
+
     static int findLength(int a, int b) {
         //공통조상구해서, 각 공통조상까지 거리합 -1
-        int curA = a; int curB = b;
-
-        while (depth[a] != depth[b]){
-            if (depth[a] > depth[b])
-                a = parent[a];
-            else if (depth[a] < depth[b])
-                b = parent[b];
+        int curA  = a; int curB = b;
+        if (depth[a] < depth[b]){ //a를 더 크게
+            int temp = a;
+            a = b;
+            b = temp;
         }
 
-        while (a!=b) {
-            a = parent[a];
-            b = parent[b];
+
+        for (int h = 17; h >= 0 ; h--) {
+            if (depth[a] - depth[b] >= (1<<h))
+                a = parent[h][a];
+        }
+        //일치해지기 직전
+        int ancestor = a;
+        if (a != b) {
+            for (int h = 17; h >= 0; h--) {
+                if (parent[h][a] != parent[h][b]) {
+                    a = parent[h][a];
+                    b = parent[h][b];
+                }
+            }
+            ancestor = parent[0][a];
         }
 
-        int ancesterDepth = depth[a];
+
+        int ancesterDepth = depth[ancestor];
         int len1 = depth[curA] - ancesterDepth +1;
         int len2 = depth[curB] - ancesterDepth +1;
 
