c10da65a2ed88d7ded974f318eb5c066ed61d3f7
__ <127599615+min20ta@users.noreply.github.com>
2025-04-23 18:11:40 +0900

    Title: 빙산의 일각 2, Time: 99ms, Memory: 9MB, Status: Wrong Answer - Codetree

---
+++ 250423/빙산의 일각 2/the-tip-of-the-iceberg-2.java
@@ -0,0 +1,67 @@
+
+import java.util.*;
+import java.io.*;
+
+
+public class Main {
+
+
+    static int[] arr;
+    static int[] depth ;
+    static boolean[] visit;
+    static int time = 0;
+    static int[] dx = {0, 1, -1, 0};
+    static int[] dy = {-1, 0,0, 1};
+    static int n;
+    static int m;
+    static int b;
+    static int end;
+    static int [] p;
+    static int [] s;
+
+
+    static int max = Integer.MIN_VALUE;
+    static int hmax = Integer.MIN_VALUE;
+    static int min = Integer.MAX_VALUE;
+
+
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        arr = new int[n];
+        for (int i = 0; i < n; i++) {
+             arr[i]= Integer.parseInt(br.readLine());
+             max = Math.max(max,arr[i]);
+        }
+
+        for (int h = 1; h <= max ; h++) {
+            hmax = Math.max(hmax,findGroup(h));
+        }
+
+        System.out.println(hmax);
+
+        //높이-> 빙상최대(물에 잠기지 않은 것 한덩이)
+    }
+
+    static int findGroup(int h) {
+        int prev = 0;
+        int count = 0;
+
+        for (int i = 0; i < n; i++) {
+            count++;
+
+            if (arr[i] <= h)
+                count--;
+            else if (arr[i] > h && i-prev == 1){
+                prev = i;
+                count--;
+            }
+        }
+        return count;
+    }
+
+}
+
+
