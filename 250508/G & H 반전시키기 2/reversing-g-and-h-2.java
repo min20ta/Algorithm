a2ace7c3861029095b7e46e6150bdc83039c7fd0
__ <127599615+min20ta@users.noreply.github.com>
2025-05-08 01:02:29 +0900

    Title: G & H 반전시키기 2, Time: 98ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 250508/G & H 반전시키기 2/reversing-g-and-h-2.java
@@ -0,0 +1,67 @@
+
+
+
+import java.util.*;
+import java.io.*;
+
+
+public class Main {
+
+
+    static int[][] parent;
+    static int[] val;
+    static int[] depth;
+    static boolean didBomb;
+    static int time = 0;
+    static int[] dx = {0, 1, -1, 0};
+    static int[] dy = {-1, 0, 0, 1};
+    static int n;
+    static int m;
+    static int c;
+    static int ans;
+    static int [] arr;
+    static boolean[][] visit;
+    static int[] L;
+    static int[] R;
+    static int size = 0;
+    static int [][] tempArr;
+
+    static int max = Integer.MIN_VALUE;
+    static int maxVal = Integer.MIN_VALUE;
+    static int min = Integer.MAX_VALUE;
+
+
+
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+
+        String str1 = br.readLine();
+        String str2 = br.readLine();
+        char [] c1 = str1.toCharArray();
+        char [] c2 = str2.toCharArray();
+
+        int count = 0;
+        for (int i = n-1; i >= 0; i--) {
+            if (c1[i] != c2[i]) {
+                count++;
+                for (int j = i; j >= 0 ; j--) {
+                    c1[j] = (c1[j] == 'G') ? 'H' : 'G';
+                }
+            }
+        }
+
+        System.out.println(count);
+
+
+    }
+
+
+
+}
+
+
+
+
