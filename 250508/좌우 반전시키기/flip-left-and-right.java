1b178522563ee48089b2fc943cb15fcec838a133
__ <127599615+min20ta@users.noreply.github.com>
2025-05-08 00:32:13 +0900

    Title: 좌우 반전시키기, Time: 90ms, Memory: 9MB, Status: Runtime Error - Codetree

---
+++ 250508/좌우 반전시키기/flip-left-and-right.java
@@ -0,0 +1,68 @@
+
+
+
+import java.util.*;
+import java.io.*;
+
+
+public class Main {
+
+
+    static int[][] parent;
+    static int[] val;
+    static int[] depth;
+    static boolean didBomb;
+    static int time = 0;
+    static int[] dx = {0, 1, -1, 0};
+    static int[] dy = {-1, 0, 0, 1};
+    static int n;
+    static int m;
+    static int c;
+    static int ans;
+    static int [] arr;
+    static boolean[][] visit;
+    static int[] L;
+    static int[] R;
+    static int size = 0;
+    static int [][] tempArr;
+
+    static int max = Integer.MIN_VALUE;
+    static int maxVal = Integer.MIN_VALUE;
+    static int min = Integer.MAX_VALUE;
+
+
+
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+
+        arr = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int count = 0;
+        for (int i = 1; i < n; i++) {
+            if (arr[i-1] == 0){
+                arr[i-1] = 1;
+                arr[i] = 1-arr[i+1];
+                if (i+1 < n)
+                    arr[i+1] = 1- arr[i+2];
+                count++;
+            }
+        }
+
+        System.out.println(count);
+
+
+
+    }
+
+
+
+}
+
+
