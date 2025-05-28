133cce900aeb256e55f840b5d10cea5984c17cdb
__ <127599615+min20ta@users.noreply.github.com>
2025-05-28 20:36:25 +0900

    Title: 연속 부분 합의 최댓값 구하기 2, Time: 88ms, Memory: 9MB, Status: Wrong Answer - Codetree

---
+++ 250528/연속 부분 합의 최댓값 구하기 2/max-of-partial-sum-2.java
@@ -0,0 +1,69 @@
+
+
+
+import java.util.*;
+import java.io.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
+public class Main {
+
+
+    static int[][] parent;
+    static int[] val;
+    static int[] depth;
+    static boolean didBomb;
+    static int time = 0;
+    static int[] dx = {0, 1, -1, 0};
+    static int[] dy = {-1, 0, 0, 1};
+    static int n;
+    static int m;
+    static int k ;
+    static int[] arr;
+    static int [][] prefixSum;
+    static int [] distance;
+    static boolean[] visit;
+    static int[] L;
+    static int[] R;
+
+
+    static int max = Integer.MIN_VALUE;
+    static int min = Integer.MAX_VALUE;
+    static Map<Integer, Integer> map = new HashMap<>();
+    static TreeSet<Integer> set = new TreeSet<>();
+
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+
+        arr = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int num = 0;
+        for (int i = 0; i < n; i++) {
+
+            if (num < num+arr[i])
+                num += arr[i];
+            else {
+                num = 0;
+                continue;
+            }
+            max = Math.max(max,num);
+        }
+
+        System.out.println(max);
+
+
+
+    }
+}
+
+
+
+
