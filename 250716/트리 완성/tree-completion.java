63e3b6d162a0e3c1df24f49e6916cee3a4d3a086
__ <127599615+min20ta@users.noreply.github.com>
2025-07-16 00:36:39 +0900

    Title: 트리 완성, Time: 269ms, Memory: 20MB, Status: Wrong Answer - Codetree

---
+++ 250716/트리 완성/tree-completion.java
@@ -0,0 +1,94 @@
+
+
+
+import java.util.*;
+import java.io.*;
+
+
+public class Main {
+
+
+    static int[][] parent;
+    static int[] val;
+    static int[] depth;
+    static boolean didBomb;
+    static int time = 0;
+    static int[] dx = {-1,-1,0,1,1,1,0,-1};
+    static int[] dy = {0,1,1,1,0,-1,-1,-1};
+    static int n ;
+    static int m;
+    static int k ;
+    static int[] arr;
+    static int[] num;
+    static int[] orderArr;
+    static int [][] prefixSum;
+    static int [] distance;
+    static boolean[][] visit;
+    static int[] v;
+
+
+    static int max = Integer.MIN_VALUE;
+    static int minCar = Integer.MAX_VALUE;
+    static int diffMin = Integer.MAX_VALUE;
+
+    static HashMap<String, PriorityQueue<String>> graph = new HashMap<>();
+    static Deque<String> stack = new LinkedList<>();
+
+    static boolean cycle ;
+    static int ans = -1;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        arr = new int[n+1];
+        for (int i = 0; i <= n; i++) {
+            arr[i] = i;
+        }
+
+        //그룹이 총 몇개인지
+
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            union(a,b);
+        }
+
+        int ans = 0;
+        Set<Integer> set = new HashSet<>();
+
+        for (int i = 1; i < n ; i++) {
+            if (!set.contains(find(i))) {
+                set.add(arr[i]);
+            }
+        }
+
+        System.out.println(set.size()-1);
+    }
+
+
+    static void union(int a, int b) {
+        int x = find(a);
+        int y = find(b);
+
+        arr[x] = y;
+    }
+
+    static int find(int x) {
+        if (arr[x] == x)
+            return x;
+
+        return arr[x] = find(arr[x]);
+    }
+
+
+
+}
+
+
+
+
