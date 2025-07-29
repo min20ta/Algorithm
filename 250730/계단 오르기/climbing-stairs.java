28673d1475e61f7db33af8faf8a78fca93586dc9
__ <127599615+min20ta@users.noreply.github.com>
2025-07-30 02:02:36 +0900

    Title: 계단 오르기, Time: 82ms, Memory: 9MB, Status: Passed - Codetree

--- 250730/계단 오르기/climbing-stairs.java
+++ 250730/계단 오르기/climbing-stairs.java
@@ -14,16 +14,15 @@ public class Main {
         StringTokenizer st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
 
-        int [] dp = new int[n+1];
+        int [] dp = new int[1001];
         
+        dp[1] = 0;
         dp[2] = 1;
+        dp[3] = 1;
         
 
-        for (int i = 3; i <= n ; i++) {
-            if(i % 2 == 0) 
-                dp[i] = 1;
-            else 
-                dp[i] = (dp[i-1] + dp[i-2]) % 10007;   
+        for (int i = 4; i <= n ; i++) {
+                dp[i] = (dp[i-2] + dp[i-3]) % 10007;   
         }
 
         System.out.println(dp[n]);
