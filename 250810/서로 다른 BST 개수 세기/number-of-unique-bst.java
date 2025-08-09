8dd099f46503b1bde1381657d8b43c388f43ab26
__ <127599615+min20ta@users.noreply.github.com>
2025-08-10 00:15:43 +0900

    Title: 서로 다른 BST 개수 세기, Time: 95ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 250810/서로 다른 BST 개수 세기/number-of-unique-bst.java
@@ -0,0 +1,39 @@
+
+
+
+import java.util.*;
+import java.io.*;
+
+
+public class Main {
+
+
+
+    static int n ;
+
+
+
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+
+
+        int [] dp = new int[n+1];
+        dp[0] = 1;
+        dp[1] = 1;
+
+        for(int i= 2 ; i <= n ; i++) {
+            for(int j = 0 ; j < i; j++) {
+                dp[i] += dp[j]*dp[i-j-1];
+            }
+        }
+
+  
+
+        System.out.println(dp[n]);
+
+    }
+
+}
