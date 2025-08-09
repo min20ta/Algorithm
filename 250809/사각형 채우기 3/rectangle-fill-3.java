46dc2c1b881dabb00e09a08c02651b3d395ece9a
__ <127599615+min20ta@users.noreply.github.com>
2025-08-09 21:54:13 +0900

    Title: 사각형 채우기 3, Time: 87ms, Memory: 9MB, Status: Passed - Codetree

--- 250809/사각형 채우기 3/rectangle-fill-3.java
+++ 250809/사각형 채우기 3/rectangle-fill-3.java
@@ -21,15 +21,14 @@ public class Main {
 
 
         long [] dp = new long[n+1];
-      
+        dp[0] = 1;
         dp[1] = 2;
 
         for (int i = 2; i <= n ; i++) {
-            if (i == 2) {
-                dp[i] = 7;
-                continue;
-            }
-            dp[i] = (2*dp[i-1] + 4*dp[i-2]) % 1000000007;
+            dp[i] = (2*dp[i-1] + 3*dp[i-2]) % 1000000007;
+
+            for(int j = i-3 ; j >= 0 ; j--)
+                dp[i] = (dp[i] + dp[j] * 2) % 1000000007;
         }
 
         System.out.println(dp[n]);
