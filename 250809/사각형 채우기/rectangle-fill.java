62647d99948c734ca223e768d208831763c9fe80
__ <127599615+min20ta@users.noreply.github.com>
2025-08-09 21:09:00 +0900

    Title: 사각형 채우기, Time: 86ms, Memory: 9MB, Status: Passed - Codetree

--- 250809/사각형 채우기/rectangle-fill.java
+++ 250809/사각형 채우기/rectangle-fill.java
@@ -23,7 +23,7 @@ public class Main {
         dp[1] = 1;
 
         for (int i = 2; i <= n ; i++) {
-            dp[i] = dp[i-1] + dp[i-2];
+            dp[i] = (dp[i-1] + dp[i-2])%10007;
         }
 
         System.out.println(dp[n]);
