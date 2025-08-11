bdc35d33d928d272d07530d8793a6550c4a98a80
__ <127599615+min20ta@users.noreply.github.com>
2025-08-11 19:15:11 +0900

    Title: 정수 사각형 최대 합, Time: 130ms, Memory: 10MB, Status: Passed - Codetree

--- 250811/정수 사각형 최대 합/maximum-sum-path-in-square.java
+++ 250811/정수 사각형 최대 합/maximum-sum-path-in-square.java
@@ -37,7 +37,7 @@ public class Main {
         }
 
         for (int i = 1; i < n; i++) {
-            dp[0][i] = dp[0][i-1] + arr[0][i-1];
+            dp[0][i] = dp[0][i-1] + arr[0][i];
         }
 
         for (int i = 1; i < n; i++) {
