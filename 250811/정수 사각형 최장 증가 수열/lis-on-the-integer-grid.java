0f4c0ef77b7a78ce6062fa6e0d85e04339010e6b
__ <127599615+min20ta@users.noreply.github.com>
2025-08-11 21:53:46 +0900

    Title: 정수 사각형 최장 증가 수열, Time: 737ms, Memory: 34MB, Status: Passed - Codetree

--- 250811/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.java
+++ 250811/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.java
@@ -11,6 +11,7 @@ public class Main {
     static int [] dx = {1,0,-1,0};
     static int [] dy = {0,1,0,-1};
     static int n ;
+    static ArrayList<pair> list= new ArrayList<>();
 
 
 
@@ -23,13 +24,18 @@ public class Main {
         int [][] arr = new int[n][n];
         int [][] dp = new int[n][n];
 
+        int max =Integer.MIN_VALUE;
+
+
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < n; j++) {
                 arr[i][j] = Integer.parseInt(st.nextToken());
+                list.add(new pair(arr[i][j], i , j));
             }
         }
 
+        Collections.sort(list);
 
 
         //아무 시작점, 상하좌우 중 현재값보다 큰 곳, 움직칸 최대수
@@ -37,27 +43,33 @@ public class Main {
         //1 . 각 칸을 모두 시작위치로 잡은 후 bfs 실행 . n^4??
         //2. 해당칸 = 상하좌우 이전칸 중 값 작은거의 움직칸 최대수+1
 
-        int max =Integer.MIN_VALUE;
 
+
+        //중간 갱신이 문제인데....
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 dp[i][j] = 1;
             }
         }
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
+        for (int i = 0; i < list.size(); i++) {
+                int x = list.get(i).x;
+                int y = list.get(i).y;
 
                 for (int k = 0; k < 4; k++) {
-                    int px = i + dx[k];
-                    int py = j + dy[k];
+                    int nx = x + dx[k];
+                    int ny = y + dy[k];
 
-                    if (px >= 0 && px < n && py >= 0 && py < n) {
-                        if (arr[i][j] > arr[px][py]) {
-                            dp[i][j] = Math.max(dp[i][j], dp[px][py] + 1);
+                    if (nx >= 0 && nx < n && ny >= 0 && ny < n) {
+                        if (arr[x][y] < arr[nx][ny]) {
+                            dp[nx][ny] = Math.max(dp[x][y] + 1, dp[nx][ny]);
                         }
                     }
                 }
+            }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
                 max = Math.max(max, dp[i][j]);
             }
         }
@@ -67,8 +79,26 @@ public class Main {
 
     }
 
-}
+    static class pair implements Comparable<pair>{
+        int num;
+        int x;
+        int y;
 
+        pair(int num, int x, int y) {
+            this.num = num;
+            this.x = x;
+            this.y = y;
+        }
+
+        @Override
+        public int compareTo(pair p) {
+            return this.num - p.num;
+        }
+
+
+    }
+
+}
 
 
 
